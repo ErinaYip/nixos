8babaecb3cfb6a34e32b471410380862f1c19f23
ErinaYip <erinayip@outlook.com>
2026-03-05 20:52:35 +0800

    add keymaps

--- home/programs/editors/nixvim/lsp/default.nix
+++ home/programs/editors/nixvim/lsp/default.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 let
   nixFiles = builtins.filter
-    (name: !builtins.elem name [ "keymap.nix" "default.nix" ])
+    (name: name != "default.nix")
     (builtins.attrNames (builtins.readDir ./.));
 
   mkServerConfig = file:
@@ -34,4 +34,56 @@ in {
       };
     };
   } // serversConfig;
+
+  diagnostic.settings = {
+    virtual_text = true;
+    float.source = true;
+  };
+
+  lsp.keymaps = [
+    {
+      key = "gd";
+      lspBufAction = "definition";
+    }
+    {
+      key = "gD";
+      lspBufAction = "references";
+    }
+    {
+      key = "gt";
+      lspBufAction = "type_definition";
+    }
+    {
+      key = "gi";
+      lspBufAction = "implementation";
+    }
+    {
+      key = "K";
+      lspBufAction = "hover";
+    }
+    {
+      action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=-1, float=true }) end";
+      key = "<leader>k";
+    }
+    {
+      action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=1, float=true }) end";
+      key = "<leader>j";
+    }
+    {
+      action = "<CMD>LspStop<Enter>";
+      key = "<leader>lx";
+    }
+    {
+      action = "<CMD>LspStart<Enter>";
+      key = "<leader>ls";
+    }
+    {
+      action = "<CMD>LspRestart<Enter>";
+      key = "<leader>lr";
+    }
+    {
+      action = "<CMD>Lspsaga hover_doc<Enter>";
+      key = "K";
+    }
+  ];
 }
