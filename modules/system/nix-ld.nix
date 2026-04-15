bbac18fd0492b2948418d9c5d76328cdb6437047
ErinaYip <erinayip@outlook.com>
2026-04-15 17:48:27 +0800

    feat: add nix-ld module with libraries

---
+++ modules/system/nix-ld.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  pkgs,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "system";
+  name = "nix-ld";
+
+  configFn = { ... }: {
+    programs.nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        zlib
+        zstd
+        stdenv.cc.cc.lib
+      ];
+      asdasdasd = true;
+    };
+  };
+}
