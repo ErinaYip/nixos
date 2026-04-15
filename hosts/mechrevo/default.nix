e3f739f8f10e290b10b174095a2559b612dbd589
ErinaYip <erinayip@outlook.com>
2026-04-15 21:58:35 +0800

    refactor: remove unused parameters in mechrevo host

--- hosts/mechrevo/default.nix
+++ hosts/mechrevo/default.nix
@@ -30,3 +30,4 @@
 # feat: configure boot engine and nvidia prime on mechrevo
 # feat: enable nix module on mechrevo
 # refactor: restructure mechrevo host configuration
+# refactor: remove unused parameters in mechrevo host
