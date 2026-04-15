66980a266b679bda12d03071f8351e23f060ccaf
ErinaYip <erinayip@outlook.com>
2026-04-15 22:28:41 +0800

    refactor: replace networkmanager with network module

---
+++ modules/system/network.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  default,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "system";
+  name = "network";
+
+  opts = {
+    proxy = lib.erinite.mkBoolOpt true "Whether to enable system proxy.";
+  };
+
+  configFn = { ... }: {
+    networking.hostName = "nixos";
+    time.timeZone = "Asia/Shanghai";
+
+    networking.networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
+
+    users.users.${default.username}.extraGroups = [ "networkmanager" ];
+    networking.firewall.enable = true;
+  };
+}
