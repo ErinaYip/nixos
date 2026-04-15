ea4169c72bfc9ab4a0aa52b5c51a5c7751d2d281
ErinaYip <erinayip@outlook.com>
2026-04-15 21:13:54 +0800

    fix: move imports to top level in fcitx5 module

--- modules/system/fcitx5.nix
+++ modules/system/fcitx5.nix
@@ -3,3 +3,4 @@
 # refactor: move oh-my-rime import to sharedModules
 # refactor: update home config path across modules
 # feat: add fcitx5 module with rime config
+# fix: move imports to top level in fcitx5 module
