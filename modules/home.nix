0953450e6b4667ef5b11368228ed447dc1de4d76
ErinaYip <erinayip@outlook.com>
2026-04-15 22:19:40 +0800

    refactor: simplify home module with direct imports

--- modules/home.nix
+++ modules/home.nix
@@ -1,41 +1,35 @@
 {
   config,
   lib,
+  default,
+  inputs,
   ...
 }:
-let
-  cfg = config.erinite.home;
-in {
-  options.erinite.home = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable home-manager integration.";
-    };
-    username = lib.mkOption {
-      type = lib.types.str;
-      default = "demo";
-      description = "Username for home-manager.";
-    };
-    config = lib.mkOption {
-      type = lib.types.attrs;
-      default = {};
-      description = "Home Manager config pass-through.";
-    };
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  options.erinite.home.config = lib.mkOption {
+    type = lib.types.attrs;
+    default = {};
+    description = "Home Manager config pass-through.";
   };
 
-  config = lib.mkIf cfg.enable {
+  config = {
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
-      users.${cfg.username} = _: lib.mkMerge [
+
+      extraSpecialArgs = { inherit inputs default; };
+
+      users.${default.username} = _: lib.mkMerge [
         {
-          home.username = cfg.username;
-          home.homeDirectory = "/home/${cfg.username}";
           home.stateVersion = config.system.stateVersion;
           xdg.enable = true;
         }
-        cfg.config
+
+        config.erinite.home.config
       ];
     };
   };
