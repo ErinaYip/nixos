d35f2f595a150b486b0be04a45d36721a56eb217
ErinaYip <erinayip@outlook.com>
2026-04-15 21:04:09 +0800

    feat: add users module with default user config

---
+++ modules/system/users.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  default,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "system";
+  name = "users";
+
+  configFn = { ... }: {
+    users.users.${default.username} = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
+  };
+}
