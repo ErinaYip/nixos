394ea35eb53eaaee1fdeb6307e32c35629b57c78
ErinaYip <erinayip@outlook.com>
2026-04-15 17:27:20 +0800

    feat: add locale and fcitx5 configuration

---
+++ modules/system/locale.nix
@@ -0,0 +1,28 @@
+{pkgs, ...}: {
+  config = {
+    i18n = {
+      defaultLocale = "zh_CN.UTF-8";
+      inputMethod = {
+        enable = true;
+        type = "fcitx5";
+        fcitx5.addons = with pkgs; [
+          fcitx5-gtk
+          qt6Packages.fcitx5-chinese-addons
+          fcitx5-rime
+        ];
+      };
+      extraLocaleSettings = {
+        LC_ADDRESS = "zh_CN.UTF-8";
+        LC_IDENTIFICATION = "zh_CN.UTF-8";
+        LC_MEASUREMENT = "zh_CN.UTF-8";
+        LC_MONETARY = "zh_CN.UTF-8";
+        LC_NAME = "zh_CN.UTF-8";
+        LC_NUMERIC = "zh_CN.UTF-8";
+        LC_PAPER = "zh_CN.UTF-8";
+        LC_TELEPHONE = "zh_CN.UTF-8";
+        LC_TIME = "zh_CN.UTF-8";
+      };
+      supportedLocales = ["zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8"];
+    };
+  };
+}
