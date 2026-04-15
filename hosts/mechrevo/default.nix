b54440d502c1712bf34dd78943f399c5e7d27520
ErinaYip <erinayip@outlook.com>
2026-04-15 20:56:03 +0800

    feat: enable nix module on mechrevo

--- hosts/mechrevo/default.nix
+++ hosts/mechrevo/default.nix
@@ -28,3 +28,4 @@
 # feat: add mechrevo host config with nvidia prime
 # chore: update git user config for mechrevo host
 # feat: configure boot engine and nvidia prime on mechrevo
+# feat: enable nix module on mechrevo
