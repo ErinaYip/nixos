27bc5910f6bab31aee261f4c52192a2106d7f191
ErinaYip <erinayip@outlook.com>
2026-04-15 17:39:17 +0800

    feat: add fcitx5 module with rime config

---
+++ modules/system/fcitx5.nix
@@ -0,0 +1,90 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "system";
+  name = "fcitx5";
+
+  configFn = { ... }: {
+    i18n.inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx5-gtk
+        qt6Packages.fcitx5-chinese-addons
+        fcitx5-rime
+      ];
+    };
+
+    erinite.home.config = let
+      wanxiang = "wanxiang-lts-zh-hans";
+      gram = pkgs.fetchurl {
+        url = "https://cnb.cool/Mintimate/rime/oh-my-rime/-/releases/download/latest/${wanxiang}.gram";
+        hash = "sha256-VVCFbxHxNxvGfnPBXolG9/lm4WnDjZl6LGneL+FmVZo=";
+      };
+    in {
+      imports = [ inputs.oh-my-rime-nix.homeModules.default ];
+
+      programs.oh-my-rime.enable = true;
+
+      wayland.windowManager.hyprland.settings.exec-once = [ 
+        "fcitx5 -d -r"
+      ];
+
+      programs.oh-my-rime.rimeConfig = {
+        schema_list = [ { schema = "rime_mint"; } ];
+        key_binder = {
+          bindings = [
+            { when = "composing"; accept = "Control+p"; send = "Up"; }
+            { when = "composing"; accept = "Control+n"; send = "Down"; }
+            { when = "composing"; accept = "Shift+Tab"; send = "Up"; }
+            { when = "composing"; accept = "Tab"; send = "Down"; }
+            { when = "paging"; accept = "minus"; send = "Page_Up"; }
+            { when = "has_menu"; accept = "equal"; send = "Page_Down"; }
+            { when = "always"; accept = "Control+Shift+1"; select = ".next"; }
+          ];
+        };
+        switcher = {
+          caption = "";
+          hotkeys = [ "Control+grave" ];
+        };
+      };
+
+      home.file.".local/share/fcitx5/rime/${wanxiang}.gram" = {
+        source = gram;
+      };
+
+      programs.oh-my-rime.schemas."rime_mint.custom.yaml".patch = {
+        "speller/algebra/+" = [
+          # "erase/^xx$/"
+          # "derive/^([zcs])h/$1/" # zh, ch, sh => z, c, s
+          # "derive/^([zcs])([^h])/$1h$2/" # z, c, s => zh, ch, sh
+          "derive/([ei])n$/$1ng/" # en => eng, in => ing
+          "derive/([ei])ng$/$1n/" # eng => en, ing => in
+          "derive/([iu])an$/$lan/"  # ian => iang, uan => uang
+          "derive/([iu])ang$/$lan/" # iang => ian, uang => uan
+          "derive/([aeiou])ng$/$1gn/"  # dagn => dang
+          "derive/([dtngkhrzcs])o(u|ng)$/$1o/" # zho => zhong|zhou
+          "derive/ong$/on/" # zhonguo => zhong guo
+          "abbrev/^([a-z]).+$/$1/" #简拼（首字母）
+          # "abbrev/^([zcs]h).+$/$1/" #简拼（zh, ch, sh）
+          "derive/v/u/" # u => ü
+        ];
+        # 语言模型
+        "grammar/language" = wanxiang;
+        "grammar/collocation_max_length" = 5;
+        "grammar/collocation_min_length" = 2;
+    
+        # translator 内加载
+        "translator/contextual_suggestions" = true;
+        "translator/max_homophones" = 7;
+        "translator/max_homographs" = 7;
+      };
+    };
+  };
+}
+
