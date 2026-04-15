bbf70d43ae5aec55a47b1f43dbc90622358ab4b8
ErinaYip <erinayip@outlook.com>
2026-04-15 20:13:27 +0800

    feat: add boot module with systemd-boot and grub

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,3 +1,4 @@
 # feat: add boot module with systemd-boot and grub
 # fix: move imports to top level in boot module
 # refactor: remove blank line in boot module
+# feat: add boot module with systemd-boot and grub
