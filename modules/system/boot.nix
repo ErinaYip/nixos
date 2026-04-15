32acb17ddf6cc7b4f9f57d0fff52148f13450252
ErinaYip <erinayip@outlook.com>
2026-04-15 20:24:36 +0800

    fix: move imports to top level in boot module

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -2,3 +2,4 @@
 # fix: move imports to top level in boot module
 # refactor: remove blank line in boot module
 # feat: add boot module with systemd-boot and grub
+# fix: move imports to top level in boot module
