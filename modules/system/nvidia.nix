16dbef1fec0896278b16cdf6c242625888abb33b
ErinaYip <erinayip@outlook.com>
2026-04-15 19:51:04 +0800

    feat: add nvidia module with prime support

---
+++ modules/system/nvidia.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  config,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "system";
+  name = "nvidia";
+
+  opts = {
+    prime = {
+      enable = lib.erinite.mkBoolOpt false "Enable NVIDIA PRIME Offload.";
+      nvidiaBusId = lib.erinite.mkStrOpt "" "Nvidia dGPU Bus ID (e.g., PCI:1:0:0)";
+      amdgpuBusId = lib.erinite.mkStrOpt "" "AMD iGPU Bus ID (e.g., PCI:6:0:0)";
+      intelBusId  = lib.erinite.mkStrOpt "" "Intel iGPU Bus ID (e.g., PCI:0:2:0)";
+    };
+  };
+
+  configFn = { cfg, ... }: {
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    services.xserver.videoDrivers = [ "nvidia" ];
+    nixpkgs.config.cudaSupport = true;
+
+    hardware.nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      prime = lib.mkIf cfg.prime.enable {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        nvidiaBusId = cfg.prime.nvidiaBusId;
+
+        amdgpuBusId = lib.mkIf (cfg.prime.amdgpuBusId != "") cfg.prime.amdgpuBusId;
+        intelBusId  = lib.mkIf (cfg.prime.intelBusId != "") cfg.prime.intelBusId;
+      };
+    };
+  };
+}
