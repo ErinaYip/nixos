9f163b57faa7b2934c3c7ba9b0596fdaba27ff87
ErinaYip <erinayip@outlook.com>
2026-04-15 17:39:17 +0800

    feat: add fcitx5 module with rime config

--- modules/system/fcitx5.nix
+++ modules/system/fcitx5.nix
@@ -2,3 +2,4 @@
 # fix: move imports to top level in fcitx5 module
 # refactor: move oh-my-rime import to sharedModules
 # refactor: update home config path across modules
+# feat: add fcitx5 module with rime config
