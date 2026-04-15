58a00b895de9580a778bacbf2bd96c514d5ae625
ErinaYip <erinayip@outlook.com>
2026-04-15 20:55:44 +0800

    feat: add nix module with flakes and gc

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -10,9 +10,21 @@ lib.erinite.mkModule args {
 
   configFn = { ... }: {
     system.stateVersion = default.systemStateVersion;
+
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs.config.allowUnfree = true;
+
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    nix.settings.auto-optimise-store = true;
+    nix.settings.max-jobs = 16;
 
     nix.settings.substituters = [
+      "https://mirrors.cernet.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
     ];
   };
