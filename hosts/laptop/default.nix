08a90df966261af20f6e79dd10040abb73d0095f
ErinaYip <erinayip@outlook.com>
2026-04-16 22:17:05 +0800

    refactor: simplify laptop module config

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -10,21 +10,11 @@ let
 in {
   imports = [./hardware.nix];
 
-  networking.hostName = hostName;
-  system.stateVersion = "25.11";
-
   erinite = {
-    home = {
-      enable = true;
-      username = defaults.username;
-    };
-
     system = {
       nix = enabled;
       users = enabled;
     };
-
-    desktop.hyprland = enabled;
 
     cli.git = {
       enable = true;
