34078401ab9e0ef8d99bad5d935195c2d9672173
ErinaYip <erinayip@outlook.com>
2026-04-16 15:34:25 +0800

    refactor: update kitty module config path

--- modules/cli/kitty.nix
+++ modules/cli/kitty.nix
@@ -4,3 +4,4 @@
 # refactor: update kitty module shell aliases
 # feat: add kitty module with default settings
 # feat: add kitty module with config and aliases
+# refactor: update kitty module config path
