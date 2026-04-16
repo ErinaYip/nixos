6d49b02135a9a3e63e9a5a73c6836ef58d9ae7fc
ErinaYip <erinayip@outlook.com>
2026-04-16 13:02:23 +0800

    feat: add qt module with gtk platform and adwaita-dark style

--- modules/desktop/qt.nix
+++ modules/desktop/qt.nix
@@ -1,3 +1,4 @@
 # feat: add qt module with gtk platform and adwaita-dark style
 # refactor: update qt config path in qt module
 # refactor: update home config path across modules
+# feat: add qt module with gtk platform and adwaita-dark style
