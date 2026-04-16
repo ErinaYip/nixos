754a0737a51496f7efaa65c9af6b51a33c60f150
ErinaYip <erinayip@outlook.com>
2026-04-16 13:02:16 +0800

    feat: add gtk module with adw-gtk3-dark and Tela-dracula-dark themes

--- modules/desktop/gtk.nix
+++ modules/desktop/gtk.nix
@@ -1,3 +1,4 @@
 # feat: add gtk module with adw-gtk3-dark and Tela-dracula-dark themes
 # refactor: update gtk config path in gtk module
 # refactor: update home config path across modules
+# feat: add gtk module with adw-gtk3-dark and Tela-dracula-dark themes
