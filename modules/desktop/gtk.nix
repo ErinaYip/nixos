9be18ed741c6d1f78510cd89dc7a1ae7d303b351
ErinaYip <erinayip@outlook.com>
2026-04-16 14:43:58 +0800

    refactor: update gtk config path in gtk module

--- modules/desktop/gtk.nix
+++ modules/desktop/gtk.nix
@@ -2,3 +2,4 @@
 # refactor: update gtk config path in gtk module
 # refactor: update home config path across modules
 # feat: add gtk module with adw-gtk3-dark and Tela-dracula-dark themes
+# refactor: update gtk config path in gtk module
