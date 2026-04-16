4dcf3224d32414f749dbbc5cc093f32772a0a49c
ErinaYip <erinayip@outlook.com>
2026-04-16 19:03:23 +0800

    feat: add gamescope and gamemode to gaming module

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -1,3 +1,4 @@
 # feat: add gaming module with steam and minecraft launchers
 # feat: add gamescope and gamemode to gaming module
 # feat: add gaming module with steam and minecraft launchers
+# feat: add gamescope and gamemode to gaming module
