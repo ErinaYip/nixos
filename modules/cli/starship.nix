69b4c91a191127a2cc86aef98271242d72ab152f
ErinaYip <erinayip@outlook.com>
2026-04-16 18:27:46 +0800

    feat: add starship plugin to yazi module

--- modules/cli/starship.nix
+++ modules/cli/starship.nix
@@ -8,3 +8,4 @@
 # refactor: remove trailing newline from starship module
 # refactor: update starship module config path
 # refactor: update home config path across modules
+# feat: add starship plugin to yazi module
