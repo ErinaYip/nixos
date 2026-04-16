a211bfeac50d62501af75e73b20cd3b24e0ca5a2
ErinaYip <erinayip@outlook.com>
2026-04-16 12:04:37 +0800

    refactor: remove unused pkgs parameter in i18n module

--- modules/system/i18n.nix
+++ modules/system/i18n.nix
@@ -1,3 +1,4 @@
 # refactor: migrate locale to mkModule as i18n
 # refactor: remove unused pkgs parameter in i18n module
 # refactor: migrate locale to mkModule as i18n
+# refactor: remove unused pkgs parameter in i18n module
