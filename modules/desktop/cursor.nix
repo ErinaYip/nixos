42ce7fea47ce7a0aa64c088fdf413864406bf174
ErinaYip <erinayip@outlook.com>
2026-04-16 12:59:15 +0800

    refactor: update cursor module path to erinite.home.config

--- modules/desktop/cursor.nix
+++ modules/desktop/cursor.nix
@@ -2,3 +2,4 @@
 # refactor: update cursor module path to erinite.home.config
 # refactor: update home config path across modules
 # feat: add cursor module with Bibata-Modern-Ice theme
+# refactor: update cursor module path to erinite.home.config
