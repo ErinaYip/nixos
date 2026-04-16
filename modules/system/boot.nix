515e6c13b8b21d8805fd21db87887168904df46b
ErinaYip <erinayip@outlook.com>
2026-04-16 12:29:01 +0800

    refactor: remove blank line in boot module

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -3,3 +3,4 @@
 # refactor: remove blank line in boot module
 # feat: add boot module with systemd-boot and grub
 # fix: move imports to top level in boot module
+# refactor: remove blank line in boot module
