4a4e388dd88cd253eff1aaa7ec890bd60e32102b
ErinaYip <erinayip@outlook.com>
2026-04-16 22:00:42 +0800

    feat: add default settings to starship module

--- modules/cli/starship.nix
+++ modules/cli/starship.nix
@@ -8,6 +8,14 @@ lib.erinite.mkModule args {
   category = "cli";
   name = "starship";
 
+  defaultSettings = {
+    add_newline = true;
+    character = {
+      success_symbol = "[›](bold green) ";
+      error_symbol = "[›](bold red) ";
+    };
+  };
+
   configFn = { settings, ... }: {
     erinite.home = {
       programs.starship = {
@@ -15,13 +23,7 @@ lib.erinite.mkModule args {
         enableBashIntegration = true;
         enableFishIntegration = true;
         enableZshIntegration = true;
-        settings = {
-          add_newline = true;
-          character = {
-            success_symbol = "[›](bold green) ";
-            error_symbol = "[›](bold red) ";
-          };
-        } // settings;
+        settings = settings;
       };
 
       programs.yazi = {
