2dae71b05cd1a9af33daeecb380f291d7604b987
ErinaYip <erinayip@outlook.com>
2026-04-16 12:02:14 +0800

    feat: enable bluetooth in network module

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -2,3 +2,4 @@
 # feat: enable bluetooth in network module
 # feat: add proxy config to network module and enable for mechrevo
 # refactor: replace networkmanager with network module
+# feat: enable bluetooth in network module
