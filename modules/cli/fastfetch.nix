96d5bdaf679283492dc95f4bb6847a2d87107db9
ErinaYip <erinayip@outlook.com>
2026-04-16 15:44:16 +0800

    refactor: update fastfetch module shell aliases

--- modules/cli/fastfetch.nix
+++ modules/cli/fastfetch.nix
@@ -1,3 +1,4 @@
 # feat: add fastfetch module with config templates
 # refactor: update fastfetch module config path
 # refactor: update home config path across modules
+# refactor: update fastfetch module shell aliases
