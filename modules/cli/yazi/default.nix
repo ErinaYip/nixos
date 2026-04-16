d4148b8e715ffe86cdd5d14edafbbdf67d003375
ErinaYip <erinayip@outlook.com>
2026-04-16 18:27:42 +0800

    feat: add yazi module with plugins and keymaps

---
+++ modules/cli/yazi/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  pkgs,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "cli";
+  name = "yazy";
+
+  configFn = { settings, ... }: {
+    erinit.home = {
+      home.packages = with pkgs; [
+        fzf
+        fd
+        ripgrep
+        zoxide
+      ];
+
+      programs.yazi = {
+        enable = true;
+        enableZshIntegration = true;
+        plugins = {
+          "smart-enter" = pkgs.yaziPlugins.smart-enter;
+        };
+
+        settings = import ./settings.nix // settings;
+        keymap = import ./keymap.nix;
+      };
+    };
+  };
+}
