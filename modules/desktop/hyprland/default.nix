159a29f5713be6164a79b7750dab247e6d3c7add
ErinaYip <erinayip@outlook.com>
2026-04-16 12:59:38 +0800

    feat: add hypridle and xdg portal config

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -10,7 +10,7 @@ lib.erinite.mkModule args {
   category = "desktop";
   name = "hyprland";
 
-  configFn = { ... }: {
+  configFn = { settings, ... }: {
     programs.hyprland = {
       enable = true;
       withUWSM = true;
@@ -25,18 +25,40 @@ lib.erinite.mkModule args {
       slurp
     ];
 
+
     erinite.home.config = lib.mkMerge [
       {
         wayland.windowManager.hyprland = {
           enable = true;
           systemd.enable = false;
+          settings = {} // settings;
         };
 
         xdg.portal = {
           enable = true;
           extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+          config = {
+            hyprland.preferred = [ "hyprland" "gtk" ];
+            common.default = [ "hyprland" "gtk" ];
+          };
         };
-      }
+
+        services.hypridle = {
+          enable = true;
+            settings = {
+              general = {
+                after_sleep_cmd = "hyprctl dispatch dpms on";
+              };
+              listener = [
+                {
+                  timeout = 600;
+                  on-timeout = "hyprctl dispatch dpms off";
+                  on-resume = "hyprctl dispatch dpms on";
+                }
+              ];
+            };
+          };
+        }
 
       (import ./settings.nix)
       (import ./binds.nix)
