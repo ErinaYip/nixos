c11f8aa0a429f41df90528184ed5a735d6716b59
ErinaYip <erinayip@outlook.com>
2026-04-16 12:28:58 +0800

    refactor: make virtualisation module options conditional

--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -1,3 +1,4 @@
 # feat: add virtualisation module and enable for mechrevo
 # refactor: make virtualisation module options conditional
 # feat: add virtualisation module and enable for mechrevo
+# refactor: make virtualisation module options conditional
