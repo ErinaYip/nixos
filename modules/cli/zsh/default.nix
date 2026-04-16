56536c1904885c09060d54d91c9671e62fcccc09
ErinaYip <erinayip@outlook.com>
2026-04-16 15:34:08 +0800

    refactor: update zsh module config path

--- modules/cli/zsh/default.nix
+++ modules/cli/zsh/default.nix
@@ -2,3 +2,4 @@
 # refactor: update zsh module config path
 # refactor: update home config path across modules
 # feat: add zsh module with fzf and completion
+# refactor: update zsh module config path
