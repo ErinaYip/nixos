862f4468ba2029bd507d575c2882cae9cc06ea81
ErinaYip <erinayip@outlook.com>
2026-04-16 12:10:27 +0800

    feat: add sddm module and enable for mechrevo

---
+++ modules/system/sddm.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  inputs,
+  pkgs,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "system";
+  name = "sddm";
+
+  imports = [ inputs.silentSDDM.nixosModules.default ];
+
+  configFn = { ... }: {
+    programs.silentSDDM = {
+      enable = true;
+      theme = "default";
+    };
+
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = false;
+
+      package = pkgs.kdePackages.sddm; 
+
+      settings = {
+        General = {
+          InputMethod = lib.mkForce ""; 
+        };
+      };
+
+      extraPackages = with pkgs.kdePackages; [
+        qtvirtualkeyboard
+        qtmultimedia
+        qt5compat
+      ];
+    };
+  };
+}
