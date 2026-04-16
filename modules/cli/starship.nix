84d10e9c31a6e60519af0f3c5e5d028bc3ec9ff9
ErinaYip <erinayip@outlook.com>
2026-04-16 15:34:19 +0800

    refactor: update starship module config path

--- modules/cli/starship.nix
+++ modules/cli/starship.nix
@@ -6,3 +6,4 @@
 # feat: add default settings to starship module
 # feat: add starship module with config templates
 # refactor: remove trailing newline from starship module
+# refactor: update starship module config path
