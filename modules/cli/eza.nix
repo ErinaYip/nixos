de33025701c5e3633af2ec11632e9e55dfb2ced6
ErinaYip <erinayip@outlook.com>
2026-04-16 15:20:06 +0800

    feat: add eza module with shell aliases

---
+++ modules/cli/eza.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "cli";
+  name = "eza";
+
+  configFn = { ... }: let
+    shellAliases = {
+      tree = "eza --tree";
+      ls = "eza";
+      ll = "eza -l";
+      la = "eza -la";
+    };
+  in {
+    programs = {
+      eza = {
+        enable = true;
+        enableBashIntegration = true;
+        enableFishIntegration = true;
+        enableZshIntegration = true;
+        extraOptions = [
+          "--color=always"
+          "--group-directories-first"
+          "--header"
+          "--time-style=long-iso"
+        ];
+        git = true;
+        icons = "always";
+      };
+      bash.shellAliases = shellAliases;
+      zsh.shellAliases = shellAliases;
+      fish.shellAliases = shellAliases;
+    };
+  };
+}
