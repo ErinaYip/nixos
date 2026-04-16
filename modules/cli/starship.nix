c075ae65eaa078c04cc510b7d0a8efaefd4c0441
ErinaYip <erinayip@outlook.com>
2026-04-16 22:00:42 +0800

    feat: add default settings to starship module

--- modules/cli/starship.nix
+++ modules/cli/starship.nix
@@ -3,3 +3,4 @@
 # refactor: update starship module config path
 # refactor: update home config path across modules
 # feat: add starship plugin to yazi module
+# feat: add default settings to starship module
