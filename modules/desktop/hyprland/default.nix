97e1adbe5e63a62e8554fc27f9791c940209ae9f
ErinaYip <erinayip@outlook.com>
2026-04-16 21:25:45 +0800

    feat: add hyprland cachix substituters

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -30,6 +30,13 @@ lib.erinite.mkModule args {
       "/share/xdg-desktop-portal"
     ];
 
+    nix.settings.substituters = [
+      "https://hyprland.cachix.org"
+    ];
+    nix.settings.trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+
     erinite.home = {
       wayland.windowManager.hyprland = {
         enable = true;
