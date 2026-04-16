73e555d91fb387025f6c370e974c072614cc8299
ErinaYip <erinayip@outlook.com>
2026-04-16 15:07:04 +0800

    feat: add starship module with config templates

---
+++ modules/cli/starship.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "cli";
+  name = "starship";
+
+  configFn = { settings, ... }: {
+    programs.starship = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableZshIntegration = true;
+      settings = {
+        add_newline = true;
+        # command_timeout = 1300;
+        # scan_timeout = 50;
+        character = {
+          success_symbol = "[›](bold green) ";
+          error_symbol = "[›](bold red) ";
+        };
+      } // settings;
+    };
+  };
+}
+
