2f512669d2d54034d057f7201be56dc265c94845
ErinaYip <erinayip@outlook.com>
2026-04-16 14:44:02 +0800

    refactor: update qt config path in qt module

--- modules/desktop/qt.nix
+++ modules/desktop/qt.nix
@@ -2,3 +2,4 @@
 # refactor: update qt config path in qt module
 # refactor: update home config path across modules
 # feat: add qt module with gtk platform and adwaita-dark style
+# refactor: update qt config path in qt module
