08c7e0a2048e279f6f4ca4471bbe35375b82611e
ErinaYip <erinayip@outlook.com>
2026-04-16 15:19:58 +0800

    feat: add zsh module with fzf and completion

---
+++ modules/cli/zsh/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  ...
+} @ args:
+
+lib.erinite.mkModule args {
+  category = "cli";
+  name = "zsh";
+
+  configFn = { ... }: {
+    programs.zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      history.size = 10000;
+
+      initContent = ''
+        ${builtins.readFile ./init.zsh}
+        ${builtins.readFile ./fzf-settings.zsh}
+        ${builtins.readFile ./fzf.zsh}
+      '';
+    };
+  };
+}
