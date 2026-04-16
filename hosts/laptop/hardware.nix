c2bf5b49412e5d0fd009258d391023fa84395aa7
ErinaYip <erinayip@outlook.com>
2026-04-16 22:56:03 +0800

    remove laptop host configuration

--- hosts/laptop/hardware.nix
+++
@@ -1,9 +0,0 @@
-{...}: {
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-}
