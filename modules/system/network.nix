926fbec7fafc0b1419ee5a2422925c4f7c1d7ee0
ErinaYip <erinayip@outlook.com>
2026-04-17 19:12:56 +0800

    refactor: use hostName parameter in network module

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -4,3 +4,4 @@
 # refactor: replace networkmanager with network module
 # feat: enable bluetooth in network module
 # feat: add proxy config to network module and enable for mechrevo
+# refactor: use hostName parameter in network module
