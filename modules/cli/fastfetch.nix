e32122e9866b73820420e4ba49156141b0a1affa
ErinaYip <erinayip@outlook.com>
2026-04-17 19:12:52 +0800

    chore: comment out fastfetch random logo alias

--- modules/cli/fastfetch.nix
+++ modules/cli/fastfetch.nix
@@ -6,3 +6,4 @@
 # refactor: update fastfetch module config path
 # refactor: update home config path across modules
 # refactor: update fastfetch module shell aliases
+# chore: comment out fastfetch random logo alias
