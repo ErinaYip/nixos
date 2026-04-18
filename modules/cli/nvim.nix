bef193093030b95fd3cb7ebb90380682d70bf00a
ErinaYip <erinayip@outlook.com>
2026-04-18 20:09:56 +0800

    fix: fix nvim

--- modules/cli/nvim.nix
+++ modules/cli/nvim.nix
@@ -8,21 +8,12 @@ lib.erinite.mkModule args {
   category = "cli";
   name = "nvim";
 
-  defaultSettings = {
-    plugins = {
-      dap.enable = false;
-      dap-ui.enable = false;
-      dap-lldb.enable = false;
-      dap-virtual-text.enable = false;
-    };
-  };
-
   configFn = { settings, ... }: {
     home-manager.sharedModules = [ inputs.erina-vim.homeModules.default ];
 
     erinite.home.programs.erina-vim = {
       enable = true;
-    } // settings;
+    };
 
     environment.shellAliases = {
       vim = "nvim";
