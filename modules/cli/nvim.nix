abab5243350fe3bdb422566b880473af0fe1af58
ErinaYip <erinayip@outlook.com>
2026-04-19 22:06:39 +0800

    feat: add nvim dap settings and merge config

--- modules/cli/nvim.nix
+++ modules/cli/nvim.nix
@@ -4,16 +4,29 @@
   ...
 } @ args:
 
-lib.erinite.mkModule args {
+with lib.erinite; mkModule args {
   category = "cli";
   name = "nvim";
+
+  defaultSettings = {
+    modules = {
+      dap = {
+        enable = false;
+        dap-ui = false;
+        dap-lldb = false;
+      };
+    };
+  };
 
   configFn = { settings, ... }: {
     home-manager.sharedModules = [ inputs.erina-vim.homeModules.default ];
 
-    erinite.home.programs.erina-vim = {
-      enable = true;
-    };
+    erinite.home.programs.erina-vim = lib.mkMerge [
+      {
+        enable = true;
+      }
+      settings
+    ];
 
     environment.shellAliases = {
       vim = "nvim";
