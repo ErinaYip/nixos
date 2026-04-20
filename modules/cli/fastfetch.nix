5b244f76025d7f99d273cf5a0b8c951a3314244d
ErinaYip <erinayip@outlook.com>
2026-04-20 21:24:14 +0800

    fix: update fastfetch module layout and improve display configuration

--- modules/cli/fastfetch.nix
+++ modules/cli/fastfetch.nix
@@ -19,38 +19,39 @@ with lib.erinite; mkModule args {
           };
           display.separator = " ";
           modules = [
-            { key = "╭────────────╮"; type = "custom"; }
-            { key = "│ {#31} user     {#keys}│"; type = "title"; }
-            { key = "├────────────┤"; type = "custom"; }
-            { key = "│ {#32}󰅐 uptime   {#keys}│"; type = "uptime"; }
-            { key = "│ {#33}󱄅 distro   {#keys}│"; type = "os"; }
-            { key = "│ {#34} kernel   {#keys}│"; type = "kernel"; }
+            { key = "╭───────────────╮"; type = "custom"; }
+            { key = "│ {#31} user        {#keys}│"; type = "title"; }
+            { key = "├───────────────┤"; type = "custom"; }
+            { key = "│ {#32}󰅐 uptime      {#keys}│"; type = "uptime"; }
+            { key = "│ {#33}󱄅 distro      {#keys}│"; type = "os"; }
+            { key = "│ {#34} kernel      {#keys}│"; type = "kernel"; }
+            { key = "│ {#35}󰏖 generations {#keys}│"; type = "command"; text = "readlink /nix/var/nix/profiles/system | cut -d- -f2"; }
             # { key = "│ {#35}󰏖 packages {#keys}│"; type = "packages"; } # very bad performance
-            { key = "│ {#36} shell    {#keys}│"; type = "shell"; }
-            { key = "│ {#32}󰩟 IP       {#keys}│"; type = "localip"; }
-            { key = "├────────────┤"; type = "custom"; }
-            { key = "│ {#33} WM       {#keys}│"; type = "wm"; }
-            { key = "│ {#34}󰓸 wmtheme  {#keys}│"; type = "wmtheme"; }
-            { key = "│ {#35}󰇄 theme    {#keys}│"; type = "theme"; }
-            { key = "│ {#36}󰉼 desktop  {#keys}│"; type = "de"; }
-            { key = "│ {#32} icons    {#keys}│"; type = "icons"; }
-            { key = "│ {#33} font     {#keys}│"; type = "font"; }
-            { key = "│ {#34} Termfont {#keys}│"; type = "terminalfont"; }
-            { key = "│ {#35}󰆿 cursor   {#keys}│"; type = "cursor"; }
-            { key = "│ {#36} icons    {#keys}│"; type = "terminal"; }
-            { key = "├────────────┤"; type = "custom"; }
-            { key = "│ {#32} battery  {#keys}│"; type = "battery"; }
-            { key = "│ {#33} CPU      {#keys}│"; type = "cpu"; }
-            { key = "│ {#34} GPU {1}    {#keys}│"; type = "gpu"; }
-            { key = "│ {#35} memory   {#keys}│"; type = "memory"; }
-            { key = "│ {#36}󰉉 disk     {#keys}│"; type = "disk"; folders = "/"; }
-            { key = "│ {#32}󰓡 swap     {#keys}│"; type = "swap"; }
-            { key = "├────────────┤"; type = "custom"; }
-            { key = "│ {#33}󰍹 {2:2}       {#keys}│"; type = "display"; }
-            { key = "├────────────┤"; type = "custom"; }
-            { key = "│ {#34}󱦟 OS Age   {#keys}│"; type = "command"; text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days"; }
-            { key = "│ {#35} colors   {#keys}│"; type = "colors"; symbol = "circle"; }
-            { key = "╰────────────╯"; type = "custom"; }
+            { key = "│ {#36} shell       {#keys}│"; type = "shell"; }
+            { key = "│ {#32}󰩟 IP          {#keys}│"; type = "localip"; }
+            { key = "├───────────────┤"; type = "custom"; }
+            { key = "│ {#33} WM          {#keys}│"; type = "wm"; }
+            { key = "│ {#34}󰓸 wmtheme     {#keys}│"; type = "wmtheme"; }
+            { key = "│ {#35}󰇄 theme       {#keys}│"; type = "theme"; }
+            { key = "│ {#36}󰉼 desktop     {#keys}│"; type = "de"; }
+            { key = "│ {#32} icons       {#keys}│"; type = "icons"; }
+            { key = "│ {#33} font        {#keys}│"; type = "font"; }
+            { key = "│ {#34} Termfont    {#keys}│"; type = "terminalfont"; }
+            { key = "│ {#35}󰆿 cursor      {#keys}│"; type = "cursor"; }
+            { key = "│ {#36} icons       {#keys}│"; type = "terminal"; }
+            { key = "├───────────────┤"; type = "custom"; }
+            { key = "│ {#32} battery     {#keys}│"; type = "battery"; }
+            { key = "│ {#33} CPU         {#keys}│"; type = "cpu"; }
+            { key = "│ {#34} GPU {1}       {#keys}│"; type = "gpu"; }
+            { key = "│ {#35} memory      {#keys}│"; type = "memory"; }
+            { key = "│ {#36}󰉉 disk        {#keys}│"; type = "disk"; folders = "/"; }
+            { key = "│ {#32}󰓡 swap        {#keys}│"; type = "swap"; }
+            { key = "├───────────────┤"; type = "custom"; }
+            { key = "│ {#33}󰍹 {2:2}          {#keys}│"; type = "display"; }
+            { key = "├───────────────┤"; type = "custom"; }
+            { key = "│ {#34}󱦟 OS Age      {#keys}│"; type = "command"; text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days"; }
+            { key = "│ {#35} colors      {#keys}│"; type = "colors"; symbol = "circle"; }
+            { key = "╰───────────────╯"; type = "custom"; }
             "break"
           ];
         };
@@ -59,7 +60,7 @@ with lib.erinite; mkModule args {
 
     (mkShellAliases {
       aliases = {
-        # fastfetch = "fastfetch --logo $(find ${../../asstes/fastfetch-icons} -type f | shuf -n 1)";
+        fastfetch = "fastfetch --logo $(find ${../../asstes/fastfetch-icons} -type f | shuf -n 1)";
         ff = "fastfetch";
       };
       shells = [ "zsh" ];
