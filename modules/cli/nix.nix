c18df4ed538a38130f749f8a9e5f023ac966ff25
ErinaYip <erinayip@outlook.com>
2026-04-20 20:12:50 +0800

    fix: improve alias configuration in nix module for better shell command shortcuts

--- modules/cli/nix.nix
+++ modules/cli/nix.nix
@@ -10,15 +10,19 @@ with lib.erinite; mkModule args {
 
   configFn = { ... }:
     mkShellAliases {
-      aliases = builtins.listToAttrs (map (x: {
-        name = "sn${x.key}";
-        value = "sudo nixos-rebuild ${x.cmd} --flake .#${hostName}";
-      }) [
-        { key = "s"; cmd = "switch"; }
-        { key = "b"; cmd = "boot"; }
-        { key = "t"; cmd = "test"; }
-        { key = "u"; cmd = "build"; }
-      ]);
+      aliases =
+        builtins.listToAttrs (map (x: {
+          name = "sn${x.key}";
+          value = "sudo nixos-rebuild ${x.cmd} --flake .#${hostName}";
+        }) [
+          { key = "s"; cmd = "switch"; }
+          { key = "b"; cmd = "boot"; }
+          { key = "t"; cmd = "test"; }
+          { key = "u"; cmd = "build"; }
+        ])
+        // {
+          snr = "nix flake update && sudo nixos-rebuild switch --flake .#${hostName}";
+        };
       shells = [ "zsh" ];
     };
 }
