486aa969a720b81d5f95ab77257da1911c8729d4
ErinaYip <erinayip@outlook.com>
2026-04-20 19:48:42 +0800

    fix: refactor nix module to use mkShellAliases for shell command shortcuts

--- modules/cli/nix.nix
+++ modules/cli/nix.nix
@@ -1,15 +1,24 @@
 {
   lib,
+  hostName,
   ...
 } @ args:
 
-lib.erinite.mkModule args {
+with lib.erinite; mkModule args {
   category = "cli";
   name = "nix";
 
-  configFn = { ... }: {
-    environment.shellAliases = {
-      sns = "sudo nixos-rebuild switch --flake";
+  configFn = { ... }:
+    mkShellAliases {
+      aliases = builtins.listToAttrs (map (x: {
+        name = "sn${x.key}";
+        value = "sudo nixos-rebuild ${x.cmd} --flake .#${hostName}";
+      }) [
+        { key = "s"; cmd = "switch"; }
+        { key = "b"; cmd = "boot"; }
+        { key = "t"; cmd = "test"; }
+        { key = "u"; cmd = "build"; }
+      ]);
+      shells = [ "zsh" ];
     };
-  };
 }
