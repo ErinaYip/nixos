49c89fc1108c2190cd8863b3683ee94f4c95dbde
ErinaYip <erinayip@outlook.com>
2026-04-20 19:14:14 +0800

    feat: simplify nvim configuration by removing default settings

--- modules/cli/nvim.nix
+++ modules/cli/nvim.nix
@@ -8,25 +8,12 @@ with lib.erinite; mkModule args {
   category = "cli";
   name = "nvim";
 
-  defaultSettings = {
-    modules = {
-      dap = {
-        enable = false;
-        dap-ui = false;
-        dap-lldb = false;
-      };
-    };
-  };
-
   configFn = { settings, ... }: {
     home-manager.sharedModules = [ inputs.erina-vim.homeModules.default ];
 
-    erinite.home.programs.erina-vim = lib.mkMerge [
-      {
-        enable = true;
-      }
-      settings
-    ];
+    erinite.home.programs.erina-vim = {
+      enable = true;
+    };
 
     environment.shellAliases = {
       vim = "nvim";
