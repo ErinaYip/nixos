d06645341b0ddfdb548b7f198f4d2a9be0e43da7
ErinaYip <erinayip@outlook.com>
2026-04-21 13:34:13 +0800

    fix: add pnpm and nodejs to system packages in configuration.nix

--- hosts/nec/configuration.nix
+++ hosts/nec/configuration.nix
@@ -1,3 +1,4 @@
 # feat: add nec host
 # fix: correct syntax for importing library in configuration.nix
 # fix: add qq to system packages in configuration.nix
+# fix: add pnpm and nodejs to system packages in configuration.nix
