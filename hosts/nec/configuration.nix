3bcd4705b9fec984b93369698a8a724e781d16f2
ErinaYip <erinayip@outlook.com>
2026-04-24 10:39:54 +0800

    feat: remove codex pkg in nec configurations

--- hosts/nec/configuration.nix
+++ hosts/nec/configuration.nix
@@ -2,3 +2,4 @@
 # fix: correct syntax for importing library in configuration.nix
 # fix: add qq to system packages in configuration.nix
 # fix: add pnpm and nodejs to system packages in configuration.nix
+# feat: remove codex pkg in nec configurations
