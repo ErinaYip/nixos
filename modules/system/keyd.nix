34329410863c004aa09c69bb2ff961c340e400de
ErinaYip <erinayip@outlook.com>
2026-04-27 13:35:34 +0800

    refactor: simplify keyd module configuration by removing defaultSettings

--- modules/system/keyd.nix
+++ modules/system/keyd.nix
@@ -2,3 +2,4 @@
 # feat: add default settings to keyd module
 # feat: add keyd module and enable for mechrevo
 # refactor: resolve merge conflicts in modules
+# refactor: simplify keyd module configuration by removing defaultSettings
