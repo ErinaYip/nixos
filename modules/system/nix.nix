c370a66153a05754d25b199f6c21457b3678a38f
ErinaYip <erinayip@outlook.com>
2026-04-27 14:17:08 +0800

    feat(nix): add Cachix as a trusted user and public key for Nix settings

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -33,7 +33,13 @@ lib.erinite.mkModule args {
       "https://mirror.nju.edu.cn/nix-channels/store"
       "https://mirrors.cernet.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://nix-community.cachix.org"
       "https://cache.nixos.org"
+    ];
+
+    nix.settings.trusted-users = [ default.username ];
+    nix.settings.trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
 
     programs.direnv.enable = true;
