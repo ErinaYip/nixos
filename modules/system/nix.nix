39b66b57c110a58a6c17adfcd49ad88b5226e568
ErinaYip <erinayip@outlook.com>
2026-04-27 13:53:49 +0800

    ops(nix): add additional Nix store mirrors

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -29,6 +29,8 @@ lib.erinite.mkModule args {
       "GOPROXY=https://goproxy.cn,direct"
     ];
     nix.settings.substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirror.nju.edu.cn/nix-channels/store"
       "https://mirrors.cernet.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
