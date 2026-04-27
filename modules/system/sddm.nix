b7075385512a8aaa9a6fef82f02a7a1ef19adc7f
ErinaYip <erinayip@outlook.com>
2026-04-27 13:35:28 +0800

    feat: enable Wayland support for SDDM display manager

--- modules/system/sddm.nix
+++ modules/system/sddm.nix
@@ -2,3 +2,4 @@
 # feat: enable wayland in sddm module
 # feat: add sddm module and enable for mechrevo
 # feat: enable wayland in sddm module
+# feat: enable Wayland support for SDDM display manager
