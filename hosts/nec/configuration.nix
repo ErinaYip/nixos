fcbb2173b2d3c7d990692c0390443df76b59f417
ErinaYip <erinayip@outlook.com>
2026-04-28 14:16:07 +0800

    fix(laptop): update TLP settings for better power management

--- hosts/nec/configuration.nix
+++ hosts/nec/configuration.nix
@@ -36,10 +36,7 @@ with eriniteLib; {
   ];
 
   boot.kernelParams = [
-    "i915.enable_fbc=1"       # 开启帧缓冲压缩 (省显存带宽和电量)
-    "i915.enable_psr=1"       # 开启面板自刷新 (画面静止时GPU休眠，极度省电)
-    "i915.enable_guc=2"       # 开启 GuC/HuC 固件加载 (优化视频解码和电源管理)
-    "pcie_aspm=force"         # 强制开启 PCIe 链路节能
+    "pcie_aspm=force"
     "intel_pstate=enable"
     "intel_idle.max_cstate=4"
     "drm.vblank_mode=1"
@@ -47,10 +44,8 @@ with eriniteLib; {
     "quiet" "splash"
     "nosmt"
   ];
-  hardware.enableRedistributableFirmware = true;
 
   services.thermald.enable = true;
-  powerManagement.powertop.enable = true;
 
   boot.blacklistedKernelModules = [
     "btusb" "bluetooth"
