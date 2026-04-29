d1182ef1ff383d9dd83a3e2d7023e4058a0c1728
ErinaYip <erinayip@outlook.com>
2026-04-29 21:54:10 +0800

    refactor(dynamic-cursors): remove unused shake configuration

--- modules/desktop/hyprland/dynamic-cursors.nix
+++ modules/desktop/hyprland/dynamic-cursors.nix
@@ -1,2 +1,3 @@
 # refactor: rename lib.erinite to eriniteLib and add hypr-dynamic-cursors
 # build(hyprland): update Hyprland and configure dynamic-cursors
+# refactor(dynamic-cursors): remove unused shake configuration
