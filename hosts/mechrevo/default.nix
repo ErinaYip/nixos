1dc7427d22ed27f3d472c3e4b94edfc4f91dd27c
ErinaYip <erinayip@outlook.com>
2026-04-30 15:41:09 +0800

    feat(hyprland): add additional monitor settings and workspace configurations

--- hosts/mechrevo/default.nix
+++ hosts/mechrevo/default.nix
@@ -42,11 +42,15 @@ with eriniteLib; {
       hyprland = {
         settings = {
           monitor = [
+            "eDP-1, preferred, 1920x0, 1.6, transform, 1"
             "eDP-2, preferred, 1920x0, 1.6, transform, 1"
+            "DP-2, 1920x1080@260.00Hz, 0x0, 1"
             "DP-3, 1920x1080@260.00Hz, 0x0, 1"
           ];
           workspace = [
+            "1, monitor:DP-2,      default:true"
             "1, monitor:DP-3,      default:true"
+            "2, monitor:eDP-1,     default:true"
             "2, monitor:eDP-2,     default:true"
             "2, layout:dwindle"
           ];
