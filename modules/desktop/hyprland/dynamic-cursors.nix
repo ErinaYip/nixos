597a4e8dfa5249e590d20f304361af753f849a6d
ErinaYip <erinayip@outlook.com>
2026-05-01 20:26:16 +0800

    feat: disable dynamic cursors shake

--- modules/desktop/hyprland/dynamic-cursors.nix
+++ modules/desktop/hyprland/dynamic-cursors.nix
@@ -1,3 +1,4 @@
 # refactor: rename lib.erinite to eriniteLib and add hypr-dynamic-cursors
 # build(hyprland): update Hyprland and configure dynamic-cursors
 # refactor(dynamic-cursors): remove unused shake configuration
+# feat: disable dynamic cursors shake
