961e21a2dbf1346e9026dcd042b6590353e470bc
ErinaYip <erinayip@outlook.com>
2026-05-02 18:35:02 +0800

    feat(nixos): enable PipeWire services and add ALSA support

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -1,3 +1,4 @@
 # feat: add sound module and enable for mechrevo
 # feat: add sound module and enable for mechrevo
 # refactor: rename lib.erinite to eriniteLib and add hypr-dynamic-cursors
+# feat(nixos): enable PipeWire services and add ALSA support
