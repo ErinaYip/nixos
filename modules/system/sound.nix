faf2e24f20fc89a684a03b0538db9b3a7b308bd1
ErinaYip <erinayip@outlook.com>
2026-05-02 18:35:02 +0800

    feat(nixos): enable PipeWire services and add ALSA support

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -9,9 +9,13 @@ eriniteLib.mkModule args {
   name = "sound";
 
   configFn = { ... }: {
+    security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
       pulse.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      wireplumber.enable = true;
     };
   };
 }
