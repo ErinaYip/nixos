b510e9d6ef2f2a4a18651edcec2fbebc586ceeb6
ErinaYip <erinayip@outlook.com>
2026-05-10 12:37:51 +0800

    refactor(zsh): set zsh to nix-shell default shell

--- modules/cli/zsh/default.nix
+++ modules/cli/zsh/default.nix
@@ -5,3 +5,4 @@
 # refactor: update zsh module config path
 # refactor: update home config path across modules
 # refactor: rename lib.erinite to eriniteLib and add hypr-dynamic-cursors
+# refactor(zsh): set zsh to nix-shell default shell
--- modules/cli/zsh/init.zsh
+++ modules/cli/zsh/init.zsh
@@ -1,2 +1,3 @@
 # feat: add zsh module with fzf and completion
 # feat: add zsh module with fzf and completion
+# refactor(zsh): set zsh to nix-shell default shell
