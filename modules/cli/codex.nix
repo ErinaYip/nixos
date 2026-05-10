2e8bfe3e073771bcbb64e47c4f798b26b96db576
ErinaYip <erinayip@outlook.com>
2026-05-10 20:16:04 +0800

    feat(codex): update model version to gpt-5.5

--- modules/cli/codex.nix
+++ modules/cli/codex.nix
@@ -1,29 +1,27 @@
-{
-  eriniteLib,
-  ...
-} @ args:
+{eriniteLib, ...} @ args:
+with eriniteLib;
+  mkModule args {
+    category = "cli";
+    name = "codex";
 
-with eriniteLib; mkModule args {
-  category = "cli";
-  name = "codex";
-
-  configFn = { ... }: {
-    erinite.home.programs.codex = {
-      enable = true;
-      settings = {
-        model = "gpt-5.4";
-        model_provider = "xem";
-        model_providers = {
-          codeboy = {
-            name = "codeboy";
-            base_url = "https://api-be.codeboy.site/v1";
-          };
-          xem = {
-            name = "xem";
-            base_url = "http://new.xem8k5.top:3000/v1";
+    configFn = _: {
+      erinite.home.programs.codex = {
+        enable = true;
+        settings = {
+          model = "gpt-5.5";
+          model_reasoning_effort = "medium";
+          model_provider = "xem";
+          model_providers = {
+            codeboy = {
+              name = "codeboy";
+              base_url = "https://api-be.codeboy.site/v1";
+            };
+            xem = {
+              name = "xem";
+              base_url = "http://new.xem8k5.top:3000/v1";
+            };
           };
         };
       };
     };
-  };
-}
+  }
