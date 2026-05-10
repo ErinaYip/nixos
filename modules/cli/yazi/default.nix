57f742f2f9e5ae677b30fa78236d22c4c9b83102
ErinaYip <erinayip@outlook.com>
2026-05-10 21:45:41 +0800

    refactor(yazi): remove redundant settings assignment

--- modules/cli/yazi/default.nix
+++ modules/cli/yazi/default.nix
@@ -7,3 +7,4 @@
 # refactor: resolve merge conflicts in modules
 # refactor: rename lib.erinite to eriniteLib and add hypr-dynamic-cursors
 # Refactor with Alejandra
+# refactor(yazi): remove redundant settings assignment
