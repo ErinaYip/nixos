420ae2a031822f0d5e9ddeef34bf3788880b97af
ErinaYip <erinayip@outlook.com>
2026-05-12 17:32:07 +0800

    feat(nemo): update configuration to include udisks2 and udiskie services

--- modules/desktop/nemo.nix
+++ modules/desktop/nemo.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   pkgs,
   eriniteLib,
   ...
@@ -8,12 +7,38 @@ eriniteLib.mkModule args {
   category = "desktop";
   name = "nemo";
 
-  configFn = {...}: {
+  configFn = _: {
     environment.systemPackages = with pkgs; [
       nemo-with-extensions
       nemo-fileroller
       gvfs
       file-roller
     ];
+
+    services.udisks2.enable = true;
+    xdg.mime.defaultApplications = {
+      "inode/directory" = ["nemo.desktop"];
+      "application/x-gnome-saved-search" = ["nemo.desktop"];
+    };
+
+    erinite.home = {
+      dconf.settings = {
+        "org/cinnamon/desktop/applications/terminal" = {
+          exec = "kitty";
+        };
+      };
+
+      services.udiskie = {
+        enable = true;
+        settings = {
+          # workaround for
+          # https://github.com/nix-community/home-manager/issues/632
+          program_options = {
+            # replace with your favorite file manager
+            file_manager = "${pkgs.nemo-with-extensions}/bin/nemo";
+          };
+        };
+      };
+    };
   };
 }
