c522f535ce601af8027773c5f967ec7636f4e748
ErinaYip <erinayip@outlook.com>
2026-05-12 17:32:07 +0800

    feat(nemo): update configuration to include udisks2 and udiskie services

--- modules/desktop/nemo.nix
+++ modules/desktop/nemo.nix
@@ -2,3 +2,4 @@
 # feat: add nemo module with extensions and gvfs
 # refactor: rename lib.erinite to eriniteLib and add hypr-dynamic-cursors
 # Refactor with Alejandra
+# feat(nemo): update configuration to include udisks2 and udiskie services
