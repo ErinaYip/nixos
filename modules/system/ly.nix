d8e01f0564a419f0fffa58f524cd9e2de99dadd0
ErinaYip <erinayip@outlook.com>
2026-05-15 14:59:48 +0800

    fix(ly): update config function syntax and add missing log path

--- modules/system/ly.nix
+++ modules/system/ly.nix
@@ -1,3 +1,4 @@
 # feat(nixos): add ly module and integrate blackhole animation file
 # feat(ly): update settings and animation file path
 # Refactor with Alejandra
+# fix(ly): update config function syntax and add missing log path
