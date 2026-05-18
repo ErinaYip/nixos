aae2878f11dca98159d8bcefed3bf92c7302a30e
ErinaYip <erinayip@outlook.com>
2026-05-18 16:25:22 +0800

    feat(codex): figured codex providers

--- modules/cli/codex.nix
+++ modules/cli/codex.nix
@@ -7,28 +7,33 @@ with eriniteLib;
     configFn = _: {
       erinite.home.programs.codex = {
         enable = true;
-        settings = {
-          model = "gpt-5.4";
-          model_reasoning_effort = "medium";
-          model_provider = "rawchat";
-          model_providers = {
-            freemodel = {
-              name = "freemodel";
-              base_url = "https://api.freemodel.dev/v1";
-              env_key = "OPENAI_API_KEY";
-            };
-            rawchat = {
-              name = "rawchat";
-              base_url = "https://rawchat.cn/codex";
-              env_key = "OPENAI_API_KEY";
-            };
-            vespervei = {
-              name = "vespervei";
-              base_url = "https://api.vespervei.com/v1";
-              env_key = "VESPERVEI_API_KEY";
-            };
-          };
-        };
+        # settings = {
+        #   model = "gpt-5.5";
+        #   model_reasoning_effort = "medium";
+        #   model_provider = "hua";
+        #   model_providers = {
+        #     freemodel = {
+        #       name = "freemodel";
+        #       base_url = "https://api.freemodel.dev/v1";
+        #       env_key = "OPENAI_API_KEY";
+        #     };
+        #     rawchat = {
+        #       name = "rawchat";
+        #       base_url = "https://rawchat.cn/codex";
+        #       env_key = "OPENAI_API_KEY";
+        #     };
+        #     hua = {
+        #       name = "hua";
+        #       base_url = "https://huablog.ink";
+        #       env_key = "OPENAI_API_KEY";
+        #     };
+        #     vespervei = {
+        #       name = "vespervei";
+        #       base_url = "https://api.vespervei.com/v1";
+        #       env_key = "VESPERVEI_API_KEY";
+        #     };
+        #   };
+        # };
       };
     };
   }
