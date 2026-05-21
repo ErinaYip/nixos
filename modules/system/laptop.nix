a3d8906ef3274be84d9b08cedca07b72a2d9b2e8
ErinaYip <erinayip@outlook.com>
2026-05-21 16:40:39 +0800

    refactor(laptop): switch to tuned

--- modules/system/laptop.nix
+++ modules/system/laptop.nix
@@ -3,36 +3,37 @@ eriniteLib.mkModule args {
   category = "system";
   name = "laptop";
 
-  configFn = {...}: {
-    services.upower.enable = true;
-    services.tuned.enable = false;
-    services.power-profiles-daemon.enable = false;
-
-    services.tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
-
-        CPU_BOOST_ON_BAT = 0;
-
-        WIFI_PWR_ON_BAT = "on";
-        PCIE_ASPM_ON_BAT = "powersupersave";
-
-        SOUND_POWER_SAVE_ON_BAT = 1;
-
-        USB_AUTOSUSPEND = 1;
-
-        START_CHARGE_THRESH_BAT0 = 40;
-        STOP_CHARGE_THRESH_BAT0 = 80;
-      };
+  configFn = _: {
+    services = {
+      upower.enable = true;
+      tuned.enable = true;
     };
+
+    # services.tlp = {
+    #   enable = true;
+    #   settings = {
+    #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    #
+    #     CPU_MIN_PERF_ON_AC = 0;
+    #     CPU_MAX_PERF_ON_AC = 100;
+    #     CPU_MIN_PERF_ON_BAT = 0;
+    #     CPU_MAX_PERF_ON_BAT = 20;
+    #
+    #     CPU_BOOST_ON_BAT = 0;
+    #
+    #     WIFI_PWR_ON_BAT = "on";
+    #     PCIE_ASPM_ON_BAT = "powersupersave";
+    #
+    #     SOUND_POWER_SAVE_ON_BAT = 1;
+    #
+    #     USB_AUTOSUSPEND = 1;
+    #
+    #     START_CHARGE_THRESH_BAT0 = 40;
+    #     STOP_CHARGE_THRESH_BAT0 = 80;
+    #   };
+    # };
   };
 }
