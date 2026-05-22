7167ca31c78f501f827280bb584685710f076bd3
ErinaYip <erinayip@outlook.com>
2026-05-22 22:57:50 +0800

    refactor: update wallpaper configuration to use attribute sets for theme specialisations

--- hosts/mechrevo/default.nix
+++ hosts/mechrevo/default.nix
@@ -70,3 +70,4 @@
 # refactor: streamline host configuration and add meta information
 # feat: add theme specialisations module and integrate wallpaper generation script
 # refactor: simplify wallpaper handling in theme configurations
+# refactor: update wallpaper configuration to use attribute sets for theme specialisations
--- modules/desktop/theme-specialisations.nix
+++ modules/desktop/theme-specialisations.nix
@@ -1,2 +1,3 @@
 # feat: add theme specialisations module and integrate wallpaper generation script
 # refactor: simplify wallpaper handling in theme configurations
+# refactor: update wallpaper configuration to use attribute sets for theme specialisations
