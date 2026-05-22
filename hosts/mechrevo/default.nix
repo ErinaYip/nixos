57c5c998d1ecadaf273c0acb44684ecc7127381f
ErinaYip <erinayip@outlook.com>
2026-05-22 22:57:50 +0800

    refactor: update wallpaper configuration to use attribute sets for theme specialisations

--- hosts/mechrevo/default.nix
+++ hosts/mechrevo/default.nix
@@ -54,24 +54,22 @@ in {
         theme-specialisations = {
           enable = true;
           default = "kurogame-olive";
-          wallpapers = [
-            {
-              name = "kurogame-olive";
+          wallpapers = {
+            kurogame-olive = {
               image = pkgs.fetchurl {
                 name = "kurogame-olive.png";
                 url = "https://media-cdn-zspms.kurogame.com/pnswebsite/website2.0/images/1773936000000/6y9pm8iicrjyhd354y-17739965643372.png";
                 hash = "sha256-wdC7ZU7KChFkyQW0B5twnpq4i1nFIovfyNEklSY973I=";
               };
-            }
-            {
-              name = "kurogame-blue";
+            };
+            kurogame-blue = {
               image = pkgs.fetchurl {
                 name = "kurogame-blue.png";
                 url = "https://media-cdn-zspms.kurogame.com/pnswebsite/website2.0/images/1773936000000/ogvbi74cjtbp460lif-17739965468193.png";
                 hash = "sha256-Hl/vRLR2V0F3JRrYl0JYjxWN9KVWkFpzaWFojOXHmJQ=";
               };
-            }
-          ];
+            };
+          };
         };
       };
 
--- modules/desktop/theme-specialisations.nix
+++ modules/desktop/theme-specialisations.nix
@@ -14,7 +14,6 @@ with eriniteLib;
 
       wallpaperModule = types.submodule {
         options = {
-          name = mkStrOpt "" "Specialisation and color scheme name.";
           image = lib.mkOption {
             type = types.oneOf [types.path types.package types.str];
             description = "Wallpaper image used to generate this theme.";
@@ -38,23 +37,22 @@ with eriniteLib;
         default = "nixos-local-dark";
         description = "Wallpaper theme name to apply to the base configuration.";
       };
-      wallpapers = mkListOpt wallpaperModule [] "Wallpapers to turn into theme specialisations.";
+      wallpapers = mkAttrOpt wallpaperModule {} "Wallpapers to turn into theme specialisations.";
     };
 
     configFn = {cfg, ...}: let
       script = ../../lib/color-scheme/matugen-to-base16.py;
-      defaultWallpapers = [
-        {
-          name = "nixos-local-dark";
+      defaultWallpapers = {
+        nixos-local-dark = {
           polarity = "dark";
           image = "${pkgs.nixos-artwork.wallpapers.simple-dark-gray.src}";
           matugenScheme = "scheme-tonal-spot";
           fallbackColor = null;
-        }
-      ];
+        };
+      };
 
-      buildScheme = wallpaper:
-        pkgs.runCommand "${wallpaper.name}-base16.yaml" {
+      buildScheme = name: wallpaper:
+        pkgs.runCommand "${name}-base16.yaml" {
           nativeBuildInputs = [
             pkgs.imagemagick
             pkgs.matugen
@@ -62,77 +60,59 @@ with eriniteLib;
           ];
         } ''
           python ${script} ${lib.escapeShellArg (toString wallpaper.image)} \
-            --name ${lib.escapeShellArg wallpaper.name} \
+            --name ${lib.escapeShellArg name} \
             --polarity ${lib.escapeShellArg wallpaper.polarity} \
             --type ${lib.escapeShellArg wallpaper.matugenScheme} \
             ${lib.optionalString (wallpaper.fallbackColor != null) "--fallback-color ${lib.escapeShellArg wallpaper.fallbackColor}"} \
             --output $out
         '';
 
-      buildHomeConfig = specialisationTheme: wallpaper: let
+      buildHomeConfig = specialisationTheme: name: wallpaper: let
         priority =
           if specialisationTheme
           then lib.mkOverride 80
           else lib.mkOverride 90;
       in {
         stylix = {
-          base16Scheme = priority "${buildScheme wallpaper}";
+          base16Scheme = priority "${buildScheme name wallpaper}";
           image = priority wallpaper.image;
           polarity = priority wallpaper.polarity;
         };
 
         xdg.dataFile = {
-          "erinite-theme/name".text = priority wallpaper.name;
+          "erinite-theme/name".text = priority name;
           "erinite-theme/wallpaper".source = priority wallpaper.image;
         };
       };
 
-      wallpapersByName = lib.foldl' (
-        acc: wallpaper:
-          acc
-          // {
-            ${wallpaper.name} = wallpaper;
-          }
-      ) {} (
-        defaultWallpapers ++ cfg.wallpapers
-      );
+      wallpapers = defaultWallpapers // cfg.wallpapers;
 
       defaultWallpaper =
         if cfg.default == null
         then null
-        else wallpapersByName.${cfg.default};
-
-      wallpapers = lib.attrValues wallpapersByName;
+        else wallpapers.${cfg.default};
     in
       lib.mkMerge [
         {
           assertions = [
             {
-              assertion = lib.all (wallpaper: wallpaper.name != "") wallpapers;
-              message = "Every erinite.desktop.theme-specialisations.wallpapers entry needs a non-empty name.";
-            }
-            {
-              assertion = cfg.default == null || builtins.hasAttr cfg.default wallpapersByName;
+              assertion = cfg.default == null || builtins.hasAttr cfg.default wallpapers;
               message = "erinite.desktop.theme-specialisations.default must match one of the configured wallpaper names.";
             }
           ];
         }
 
         (lib.mkIf (defaultWallpaper != null) {
-          erinite.home = buildHomeConfig false defaultWallpaper;
+          erinite.home = buildHomeConfig false cfg.default defaultWallpaper;
         })
 
         {
-          specialisation = lib.listToAttrs (
-            map (wallpaper:
-              lib.nameValuePair wallpaper.name {
-                configuration = {
-                  erinite.home = buildHomeConfig true wallpaper;
-                  environment.etc."erinite-theme/specialisation".text = wallpaper.name;
-                };
-              })
-            wallpapers
-          );
+          specialisation = lib.mapAttrs (name: wallpaper: {
+            configuration = {
+              erinite.home = buildHomeConfig true name wallpaper;
+              environment.etc."erinite-theme/specialisation".text = name;
+            };
+          }) wallpapers;
         }
       ];
   }
