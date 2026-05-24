fa4f37f1e415d065d214408fd6859759a8fc7719
ErinaYip <erinayip@outlook.com>
2026-05-24 20:23:44 +0800

    fix: add --source-color-index 0 for matugen cmd

--- lib/color-scheme/matugen-to-base16.py
+++ lib/color-scheme/matugen-to-base16.py
@@ -1,3 +1,4 @@
 # feat: add theme specialisations module and integrate wallpaper generation script
 # refactor: update matugen scheme type to 'scheme-content' in theme specialisations
 # refactor: disabled auto enable for theme-specialisations module
+# fix: add --source-color-index 0 for matugen cmd
