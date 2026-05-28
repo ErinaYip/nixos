91476b01c0abfd5819df2a95d78ab87e3fba8d29
ErinaYip <erinayip@outlook.com>
2026-05-28 20:27:58 +0800

    feat(ghostty): add ghostty module

---
+++ home/cli/ghostty.nix
@@ -0,0 +1,35 @@
+{eriniteLib, ...} @ args:
+with eriniteLib;
+  mkModule args {
+    namespace = ["erinite" "home"];
+    category = "cli";
+    name = "ghostty";
+
+    configFn = _: {
+      programs.ghostty = {
+        enable = true;
+        enableZshIntegration = true;
+        installBatSyntax = true;
+
+        settings = {
+          command = "zsh";
+          font-family = "MapleMono NF CN";
+          cursor-style = "bar";
+
+          window-decoration = "none";
+          window-padding-x = 0;
+          window-padding-y = 0;
+
+          confirm-close-surface = false;
+          mouse-hide-while-typing = true;
+          mouse-scroll-multiplier = 0.1;
+
+          keybind = [
+            "ctrl+backspace=reset_font_size"
+            "ctrl+plus=increase_font_size:1"
+            "ctrl+equal=increase_font_size:1"
+          ];
+        };
+      };
+    };
+  }
