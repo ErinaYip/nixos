ae997e7117abc24c72ad4523160ccb0f6f343f94
ErinaYip <erinayip@outlook.com>
2026-05-29 17:15:27 +0800

    refactor(mechrevo)

--- hosts/mechrevo/home.nix
+++ hosts/mechrevo/home.nix
@@ -1,52 +1,37 @@
-{eriniteLib, ...}: with eriniteLib; {
-  programs.cava = enabled;
-
-  wayland.windowManager.hyprland = {
-    extraConfig = ''
-      local is_ext = false
-      for _, mon in ipairs(hl.get_monitors()) do
-          if mon.name == "DP-2" or mon.name == "DP-3" then
-              is_ext = true
-              break
-          end
-      end
-
-      for _, name in ipairs({"eDP-1", "eDP-2"}) do
-          hl.monitor({
-              output = name,
-              mode = "2560x1600@180.00Hz",
-              position = "1920x0",
-              scale = "1.6",
-              transform = is_ext and 1 or 0
-          })
-      end
-
-      for _, name in ipairs({"DP-2", "DP-3"}) do
-          hl.monitor({
-              output = name,
-              mode = "1920x1080@260.00Hz",
-              position = "0x0",
-              scale = "1"
-          })
-      end
-    '';
-    settings = {
-      workspace_rule = [
-        {
-          workspace = "1";
-          monitor = "eDP-1";
-          default = true;
-        }
-        {
-          workspace = "1";
-          monitor = "eDP-2";
-          default = true;
-        }
-        {
-          workspace = "1";
-          layout = "dwindle";
-        }
-      ];
-    };
+{eriniteLib, ...}:
+with eriniteLib; {
+  programs = {
+    cava = enabled;
+    vscode = enabled;
   };
+
+  wayland.windowManager.hyprland.extraConfig = ''
+    local eDP = "desc:China Star Optoelectronics Technology Co. Ltd MNG007DA5-4"
+    local DP  = "desc:HKC OVERSEAS LIMITED X5 0000000000001"
+
+    local has_ext = false
+    for _, mon in ipairs(hl.get_monitors()) do
+      if mon.description == "HKC OVERSEAS LIMITED X5 0000000000001" then
+        has_ext = true
+        hl.workspace_rule({ workspace = "1", monitor = DP, default = true, })
+        hl.workspace_rule({ workspace = "2", monitor = eDP, default = true, layout = "dwindle", })
+        break
+      end
+    end
+
+    hl.monitor({
+      output = eDP,
+      mode = "2560x1600@180.00Hz",
+      position = has_ext and "1920x0" or "0x0",
+      scale = 1.6,
+      transform = has_ext and 1 or 0,
+    })
+
+    hl.monitor({
+      output = DP,
+      mode = "1920x1080@260.00Hz",
+      position = "0x0",
+      scale = 1,
+    })
+  '';
 }
--- hosts/mechrevo/os.nix
+++ hosts/mechrevo/os.nix
@@ -44,7 +44,6 @@
     wechat
     materialgram
 
-    vscode
     obsidian
     libreoffice
   ];
