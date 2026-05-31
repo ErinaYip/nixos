44121003f11fb5df98ea6b44ba4a233f56ca12a0
ErinaYip <erinayip@outlook.com>
2026-05-31 15:07:25 +0800

    deat(mechrevo): add wallpaper

--- hosts/mechrevo/default.nix
+++ hosts/mechrevo/default.nix
@@ -20,6 +20,10 @@
           url = "https://media-cdn-zspms.kurogame.com/pnswebsite/website2.0/images/1770134400000/f8pracrulrwq5570gx-177017628557319.png";
           hash = "sha256-5EGHH5D+VYMopFQTUy4TjPkLs3MN+e4VzBiJEXHiZUM=";
         };
+        "kurogame-neon-green.png" = {
+          url = "https://media-cdn-zspms.kurogame.com/pnswebsite/website2.0/images/1764000000000/4b9a8n00pfrq0tr3ex-176404372647915.png";
+          hash = "sha256-EaqlkhrPWsYVsorKnRMT+zbAcKSuCi1NRvKtBfOIwis=";
+        };
         "kurogame-silver-orange.png" = {
           url = "https://media-cdn-zspms.kurogame.com/pnswebsite/website2.0/images/1764000000000/vfr8pepbj8kv9i2ozf-17640438278579.png";
           hash = "sha256-9PnJsPlaKSd7MyhxaoMLsK0pwtnNph6R0SSKJtshrVY=";
