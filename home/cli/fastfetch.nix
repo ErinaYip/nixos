2c19efb2840fc05d06f1e766110b3c08f457e216
ErinaYip <erinayip@outlook.com>
2026-05-31 15:06:43 +0800

    feat(fastfetch): change fastfetch icons

--- home/cli/fastfetch.nix
+++ home/cli/fastfetch.nix
@@ -1,3 +1,4 @@
 # refactor: split os and home modules
 # refactor: standardize indentation and formatting in Nix configuration files
 # feat: update zsh aliases structure and add nh module to common presets
+# feat(fastfetch): change fastfetch icons
