9364f9bea716b154f5c60687d7466e2154e92478
ErinaYip <erinayip@outlook.com>
2026-06-04 15:59:03 +0800

    fix(system): raise inotify limits

--- os/system/kernel.nix
+++ os/system/kernel.nix
@@ -15,6 +15,11 @@ with eriniteLib;
 
     configFn = {cfg, ...}: {
       boot.kernelPackages = pkgs.linuxPackages_latest;
+      boot.kernel.sysctl = {
+        "fs.inotify.max_user_watches" = 2097152;
+        "fs.inotify.max_user_instances" = 1048576;
+        "fs.inotify.max_queued_events" = 65536;
+      };
 
       services.scx = lib.mkIf cfg.sched_ext {
         enable = true;
