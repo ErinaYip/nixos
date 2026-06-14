7e7797a26418a30fa7f4be28c40e91d06786ccec
ErinaYip <erinayip@outlook.com>
2026-06-14 11:37:50 +0800

    fix(mechrove): rewrite mulitscreen decating for huprland

--- hosts/mechrevo/home.nix
+++ hosts/mechrevo/home.nix
@@ -3,32 +3,37 @@ with eriniteLib; {
   programs.cava = enabled;
 
   wayland.windowManager.hyprland.extraConfig = ''
-    local eDP = "desc:China Star Optoelectronics Technology Co. Ltd MNG007DA5-4"
-    local DP  = "desc:HKC OVERSEAS LIMITED X5 0000000000001"
-
-    local has_ext = false
-    for _, mon in ipairs(hl.get_monitors()) do
-      if mon.description == "HKC OVERSEAS LIMITED X5 0000000000001" then
-        has_ext = true
-        hl.workspace_rule({ workspace = "2", monitor = DP, default = true, })
-        hl.workspace_rule({ workspace = "1", monitor = eDP, default = true, layout = "dwindle", })
-        break
-      end
-    end
+    local eDP = "China Star Optoelectronics Technology Co. Ltd MNG007DA5-4"
+    local DP  = "HKC OVERSEAS LIMITED X5 0000000000001"
 
     hl.monitor({
-      output = eDP,
+      output = "desc:" .. eDP,
       mode = "2560x1600@180.00Hz",
-      position = "1920x0",
+      position = "0x0",
       scale = 1.6,
-      transform = has_ext and 1 or 0,
+      transform = 0,
     })
 
     hl.monitor({
-      output = DP,
+      output = "desc:" .. DP,
       mode = "1920x1080@260.00Hz",
       position = "0x0",
       scale = 1,
     })
+
+    for _, mon in ipairs(hl.get_monitors()) do
+      if mon.description == DP then
+        hl.workspace_rule({ workspace = "2", monitor = DP, default = true, })
+        hl.workspace_rule({ workspace = "1", monitor = eDP, default = true, layout = "dwindle", })
+        hl.monitor({
+          output = "desc:" .. eDP,
+          mode = "2560x1600@180.00Hz",
+          position = "1920x0",
+          scale = 1.6,
+          transform = 1,
+        })
+        break
+      end
+    end
   '';
 }
