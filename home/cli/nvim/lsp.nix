700bc817ef53a0ccf68fba8808d87abbc5629e3e
ErinaYip <erinayip@outlook.com>
2026-06-14 12:19:25 +0800

    feat(nvim): set clang-format style to google and disable copyright lint

--- home/cli/nvim/lsp.nix
+++ home/cli/nvim/lsp.nix
@@ -2,3 +2,4 @@
 # feat(nvim): add basedpyright settings for enhanced Python analysis
 # feat(nvim): add mdx language server
 # feat(nvim): add clang
+# feat(nvim): set clang-format style to google and disable copyright lint
