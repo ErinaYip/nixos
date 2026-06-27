f65a373a07c65824937fbe2cf30978a66cf63caa
ErinaYip <erinayip@outlook.com>
2026-06-27 14:12:33 +0800

    fix(nvim): snackss binding

--- home/cli/nvim/snacks.nix
+++ home/cli/nvim/snacks.nix
@@ -68,10 +68,6 @@
     };
   };
 
-  luaConfigPost = ''
-    Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-  '';
-
   keymaps = [
     (mkKeymapd ["n"] "<leader>e" "<cmd>lua Snacks.explorer()<cr>" "[E]xplorer")
     (mkKeymapd ["n"] "<leader>fw" ":lua Snacks.picker.grep()<cr>" "[F]ind content (snacks)")
@@ -79,5 +75,6 @@
     (mkKeymapd ["n"] "<leader> " ":lua Snacks.picker.smart()<cr>" "[F]ind file smartly (snacks)")
     (mkKeymapd ["n"] "<leader>fo" ":lua Snacks.picker.recent()<cr>" "[F]ind in recent file (snacks)")
     (mkKeymapd ["n"] "<leader>fh" ":lua Snacks.picker.help()<cr>" "[F]ind in [H]elp (snacks)")
+    (mkKeymapd ["n"] "<leader>uw" ":lua Snacks.toggle.option('wrap', {name='wrap'})<cr>" "[U]i toggle [W]rap (snacks)")
   ];
 }
