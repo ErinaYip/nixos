e45ddc5319ae9b96c6acd27c1d8eebe3106eaad4
ErinaYip <erinayip@outlook.com>
2026-06-28 20:52:52 +0800

    fix(nh): no specialisations

--- home/cli/nh.nix
+++ home/cli/nh.nix
@@ -1,22 +1,26 @@
 {eriniteLib, ...} @ args: let
-  inherit (builtins) elemAt concatMap listToAttrs substring;
+  inherit (builtins) elemAt listToAttrs substring;
+
+  commands = [
+    ["s" "switch"]
+    ["b" "boot"]
+    ["t" "test"]
+    ["u" "build"]
+  ];
+
+  mkAlias = target: extraArgs: cmd: {
+    name = "n${substring 0 1 target}${elemAt cmd 0}";
+    value = "nh ${target} ${elemAt cmd 1}${extraArgs}";
+  };
 in
   with eriniteLib;
     mkModule args {
       configFn = _: {
-        erinite.home.cli.zsh.aliases = listToAttrs (
-          concatMap (
-            target:
-              map (cmd: {
-                name = "n${substring 0 1 target}${elemAt cmd 0}";
-                value = "nh ${target} ${elemAt cmd 1}";
-              }) [
-                ["s" "switch"]
-                ["b" "boot"]
-                ["t" "test"]
-                ["u" "build"]
-              ]
-          ) ["os" "home"]
-        );
+        erinite.home.cli.zsh.aliases =
+          listToAttrs
+          (
+            (map (mkAlias "os" "") commands)
+            ++ (map (mkAlias "home" " --no-specialisation") commands)
+          );
       };
     }
