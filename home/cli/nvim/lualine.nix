9e804beb137a64b27cd5a965762d7974f1ecb428
ErinaYip <erinayip@outlook.com>
2026-06-28 20:52:37 +0800

    feat(nvim): enhance lualine

--- home/cli/nvim/lualine.nix
+++ home/cli/nvim/lualine.nix
@@ -56,10 +56,19 @@
         ''
       ];
       x = [
+        ''require("snacks").profiler.status()''
+        ''
+          {
+            function() return require("noice").api.status.command.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            color = function() return { fg = require("snacks").util.color("Statement") } end,
+          }
+        ''
         ''
           {
             function() return require("noice").api.status.mode.get() end,
             cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = function() return { fg = require("snacks").util.color("Constant") } end,
           }
         ''
         ''
