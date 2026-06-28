75cbc63ab1f4bdba532b4ffe0655c357f59ce09c
ErinaYip <erinayip@outlook.com>
2026-06-28 19:17:34 +0800

    feat(wechat): packed wechat using offical link

--- home/desktop/wechat.nix
+++ home/desktop/wechat.nix
@@ -3,19 +3,49 @@
   eriniteLib,
   ...
 } @ args: let
-  wechat = pkgs.wechat.overrideAttrs (oldAttrs: {
-    buildCommand =
-      oldAttrs.buildCommand
-      + ''
-        substituteInPlace "$out/share/applications/wechat.desktop" \
-          --replace-fail \
-            "Exec=wechat %U" \
-            "Exec=env XCURSOR_THEME=Bibata-Modern-Ice XCURSOR_SIZE=24 wechat --ozone-platform=wayland --force-device-scale-factor=1.6 %U" \
+  wechat = let
+    pname = "wechat";
+    version = "universal";
+
+    src = pkgs.fetchurl {
+      url = "https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.AppImage";
+      hash = "sha256-vTTkuFm1LhAqVvuynIfYdROPf19nfCQIOGhw6Z+dOeo=";
+    };
+
+    appimageContents = pkgs.appimageTools.extract {
+      inherit pname version src;
+      postExtract = ''
+        if [ -f $out/opt/wechat/wechat ]; then
+          patchelf --replace-needed libtiff.so.5 libtiff.so $out/opt/wechat/wechat || true
+        fi
+      '';
+    };
+  in
+    pkgs.appimageTools.wrapAppImage {
+      inherit pname version;
+
+      src = appimageContents;
+
+      extraInstallCommands = ''
+        mkdir -p $out/share/applications
+        mkdir -p $out/share/icons/hicolor/256x256/apps
+
+        cp ${appimageContents}/wechat.desktop $out/share/applications/
+        cp ${appimageContents}/wechat.png $out/share/icons/hicolor/256x256/apps/ || true
+
+        substituteInPlace $out/share/applications/wechat.desktop \
+          --replace-fail "AppRun" "wechat" \
           --replace-fail \
             "Name[zh_CN]=微信" \
             "Name[zh_CN]=wechat"
       '';
-  });
+
+      meta = {
+        description = "WeChat Official Linux Universal AppImage";
+        homepage = "https://linux.weixin.qq.com/";
+        platforms = ["x86_64-linux"];
+      };
+    };
 in
   eriniteLib.mkModule args {
     configFn = _: {
