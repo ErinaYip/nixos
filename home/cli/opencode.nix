a1f3c5dc4e9402fdad513253da0e8f85cdf7e68e
ErinaYip <erinayip@outlook.com>
2026-07-02 12:18:02 +0800

    feat(opencode): rewrite with full provider, skills, and settings support

--- home/cli/opencode.nix
+++ home/cli/opencode.nix
@@ -1,6 +1,50 @@
-{eriniteLib, ...} @ args:
-eriniteLib.mkModule args {
-  configFn = _: {
-    programs.opencode.enable = true;
+{
+  lib,
+  pkgs,
+  eriniteLib,
+  ...
+} @ args: let
+  mkProvider = name: npm: baseURL: models: {
+    programs.opencode.settings.provider.${name} = {
+      inherit npm name;
+      options = {
+        inherit baseURL;
+        apiKey = "{env:${lib.strings.toUpper name}_API_KEY}";
+      };
+      models = lib.genAttrs models (x: {name = x;});
+    };
   };
-}
+
+  mkOpenAIProvider = name: baseURL: (mkProvider name "@ai-sdk/openai" baseURL ["gpt-5.5"]);
+
+  ctf-skills = pkgs.fetchFromGitHub {
+    owner = "ljagiello";
+    repo = "ctf-skills";
+    rev = "1af14f9030fee9da46014a8a3ed61a555b81ab98";
+    sha256 = "sha256-v3JNLpd4JeeFdnXj219kT8BOOh+O7g/hTwaSwyIhubE=";
+  };
+in
+  with eriniteLib;
+    mkModule args {
+      configFn = _:
+        lib.mkMerge [
+          {
+            programs.opencode = {
+              enable = true;
+              skills = {
+                inherit ctf-skills;
+              };
+              settings = {
+                autoupdate = false;
+                permission = {
+                  edit = "ask";
+                  bash = "ask";
+                };
+                disabled_providers = ["openai" "gemini" "anthropic"];
+              };
+            };
+          }
+
+          (mkOpenAIProvider "hua" "https://huablog.xyz/v1")
+        ];
+    }
