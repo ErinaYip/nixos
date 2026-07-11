3a591f7d59e10a8eae765adb3e35a82f5c046b60
ErinaYip <erinayip@outlook.com>
2026-07-11 10:47:35 +0800

    fix(mechrevo): hyprland monitor settings

--- hosts/mechrevo/home.nix
+++ hosts/mechrevo/home.nix
@@ -15,61 +15,33 @@ with eriniteLib; {
     hl.monitor({
       output = "desc:" .. eDP,
       mode = "2560x1600@180.00Hz",
-      position = "1920x0",
-      scale = 1.6,
-      transform = 1,
-    })
-
-    hl.monitor({
-      output = "desc:" .. DP,
-      mode = "1920x1080@260.00Hz",
       position = "0x0",
-      scale = 1,
+      scale = 1.6,
+      transform = 0,
     })
 
     for _, mon in ipairs(hl.get_monitors()) do
       if mon.description == DP then
         hl.workspace_rule({ workspace = "2", monitor = DP, default = true, })
         hl.workspace_rule({ workspace = "1", monitor = eDP, default = true, layout = "dwindle", })
+
+        hl.monitor({
+          output = "desc:" .. eDP,
+          mode = "2560x1600@180.00Hz",
+          position = "1920x0",
+          scale = 1.6,
+          transform = 1,
+        })
+
+        hl.monitor({
+          output = "desc:" .. DP,
+          mode = "1920x1080@260.00Hz",
+          position = "0x0",
+          scale = 1,
+        })
+
         break
       end
     end
   '';
-
-  # ''
-  #   local eDP = "China Star Optoelectronics Technology Co. Ltd MNG007DA5-4"
-  #   local DP  = "HKC OVERSEAS LIMITED X5 0000000000001"
-  #
-  #   hl.monitor({
-  #     output = "desc:" .. eDP,
-  #     mode = "2560x1600@180.00Hz",
-  #     position = "0x0",
-  #     scale = 1.6,
-  #     transform = 0,
-  #   })
-  #
-  #   for _, mon in ipairs(hl.get_monitors()) do
-  #     if mon.description == DP then
-  #       hl.workspace_rule({ workspace = "2", monitor = DP, default = true, })
-  #       hl.workspace_rule({ workspace = "1", monitor = eDP, default = true, layout = "dwindle", })
-  #
-  #       hl.monitor({
-  #         output = "desc:" .. eDP,
-  #         mode = "2560x1600@180.00Hz",
-  #         position = "1920x0",
-  #         scale = 1.6,
-  #         transform = 1,
-  #       })
-  #
-  #       hl.monitor({
-  #         output = "desc:" .. DP,
-  #         mode = "1920x1080@260.00Hz",
-  #         position = "0x0",
-  #         scale = 1,
-  #       })
-  #
-  #       break
-  #     end
-  #   end
-  # '';
 }
