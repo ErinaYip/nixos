6b84adc647f47202f14e36e6ec18199176b624c1
ErinaYip <erinayip@outlook.com>
2026-08-05 14:53:44 +0800

    refactor(hyprland): window rules

--- home/desktop/hyprland/rules.nix
+++ home/desktop/hyprland/rules.nix
@@ -4,29 +4,39 @@ let
 in {
   window_rule =
     [
-      (mkRule {class = "^(steam_app_.*)$";} {
-        float = true;
-        opacity = "1.0 override 1.0 override";
-      })
+      (mkRule
+        {class = "^(steam_app_.*)$";}
+        {
+          float = true;
+          opacity = "1.0 override 1.0 override";
+        })
 
-      (mkRule {fullscreen = 1;} {
-        opacity = "1.0 override 1.0 override";
-      })
+      (mkRule
+        {fullscreen = 1;}
+        {opacity = "1.0 override 1.0 override";})
 
-      (mkRule {initial_title = ".*float.*";} {
-        float = true;
-        size = ["60%" "70%"];
-        center = true;
-      })
+      (mkRule
+        {initial_title = ".*float.*";}
+        {
+          float = true;
+          size = ["60%" "70%"];
+          center = true;
+        })
     ]
-    ++ map (title: mkRule {initial_title = title;} {float = true;}) [
+    ++ map (title:
+      mkRule
+      {initial_title = title;}
+      {float = true;})
+    [
       ".*图片和视频.*"
       ".*图片查看器.*"
       ".*视频播放器.*"
       ".*媒体查看器.*"
       ".*画中画.*"
       ".*打开文件.*"
+
       "io.github.celluloid_player.Celluloid"
+      "好友列表"
     ];
 
   layer_rule = [
