65e9ba9a9c71bc8fb3c8ac616d9cb55e07c96b59
ErinaYip <erinayip@outlook.com>
2026-08-07 16:08:58 +0800

    refactor(matugen)

--- home/desktop/matugen.nix
+++ home/desktop/matugen.nix
@@ -1,16 +1,22 @@
-{eriniteLib, ...} @ args:
+{
+  pkgs,
+  eriniteLib,
+  ...
+} @ args:
 eriniteLib.mkModule args {
   configFn = _: {
-    xdg.configFile."matugen/config.toml".text = ''
-      [config]
+    xdg.configFile."matugen/config.toml".source = pkgs.writers.writeTOML "config.toml" {
+      config.templates = {
+        millennium = {
+          input_path = ../../assets/matugen/millennium.css;
+          output_path = "~/.steam/steam/millennium/themes/Material-Theme/css/main/colors/matugen.css";
+        };
 
-      [templates.millennium]
-      input_path = '${../../assets/matugen/millennium.css}'
-      output_path = '~/.steam/steam/millennium/themes/Material-Theme/css/main/colors/matugen.css'
-
-      [templates.prismlauncher]
-      input_path = '${../../assets/matugen/prismlauncher.json}'
-      output_path = '~/.local/share/PrismLauncher/themes/Matugen/theme.json'
-    '';
+        prismlauncher = {
+          input_path = ../../assets/matugen/prismlauncher.json;
+          output_path = "~/.local/share/PrismLauncher/themes/Matugen/theme.json";
+        };
+      };
+    };
   };
 }
