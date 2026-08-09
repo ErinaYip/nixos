e47a783ced1daf98022161f59d5927cb2116f0e8
ErinaYip <erinayip@outlook.com>
2026-08-09 14:33:33 +0800

    fix(matugen): config file fotmat

--- home/desktop/matugen.nix
+++ home/desktop/matugen.nix
@@ -1,22 +1,16 @@
-{
-  pkgs,
-  eriniteLib,
-  ...
-} @ args:
+{eriniteLib, ...} @ args:
 eriniteLib.mkModule args {
   configFn = _: {
-    xdg.configFile."matugen/config.toml".source = pkgs.writers.writeTOML "config.toml" {
-      config.templates = {
-        millennium = {
-          input_path = ../../assets/matugen/millennium.css;
-          output_path = "~/.steam/steam/millennium/themes/Material-Theme/css/main/colors/matugen.css";
-        };
+    xdg.configFile."matugen/config.toml".text = ''
+      [config]
 
-        prismlauncher = {
-          input_path = ../../assets/matugen/prismlauncher.json;
-          output_path = "~/.local/share/PrismLauncher/themes/Matugen/theme.json";
-        };
-      };
-    };
+      [templates.millennium]
+      input_path = '${../../assets/matugen/millennium.css}'
+      output_path = '~/.steam/steam/millennium/themes/Material-Theme/css/main/colors/matugen.css'
+
+      [templates.prismlauncher]
+      input_path = '${../../assets/matugen/prismlauncher.json}'
+      output_path = '~/.local/share/PrismLauncher/themes/Matugen/theme.json'
+    '';
   };
 }
