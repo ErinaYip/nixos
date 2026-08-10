8a9a1818d71605cd23cfe3d43b8aa31bf2402a62
ErinaYip <erinayip@outlook.com>
2026-08-10 09:42:31 +0800

    fix(niri): avoid overlay dependency

--- home/desktop/niri/settings.nix
+++ home/desktop/niri/settings.nix
@@ -1,10 +1,13 @@
 {
   lib,
   pkgs,
+  inputs,
   ...
-}: {
+}: let
+  niriPackages = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system};
+in {
   prefer-no-csd = true;
-  xwayland-satellite.path = "${lib.getExe pkgs.xwayland-satellite-unstable}";
+  xwayland-satellite.path = "${lib.getExe niriPackages.xwayland-satellite-unstable}";
 
   layout = {
     gaps = 2;
@@ -18,20 +21,6 @@
     };
 
     preset-column-widths = builtins.genList (i: {proportion = (i + 1) * 2.0 / 7.0;}) 3;
-    # [
-    #   {proportion = 1.0 / 3.0;}
-    #   {proportion = 1.0 / 2.0;}
-    #   {proportion = 2.0 / 3.0;}
-    #   {proportion = 3.0 / 4.0;}
-    #   {proportion = 4.0 / 5.0;}
-    # ];
-
-    # tab-indicator = {
-    #   gap = 2;
-    #   width = 3;
-    #   gaps-between-tabs = 2;
-    #   corner-radius = 12;
-    # };
 
     border = {
       enable = true;
@@ -41,8 +30,6 @@
     focus-ring = {
       width = 3;
     };
-
-    # shadow.enable = true;
   };
 
   input = {
--- os/desktop/niri.nix
+++ os/desktop/niri.nix
@@ -3,32 +3,27 @@
   inputs,
   eriniteLib,
   ...
-} @ args:
-with eriniteLib;
-  mkModule args {
-    configFn = _: {
-      nixpkgs.overlays = [inputs.niri.overlays.niri];
-      programs.niri = {
-        enable = true;
-        package = pkgs.niri-unstable;
-      };
+} @ args: let
+  niriPackages = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system};
+in
+  with eriniteLib;
+    mkModule args {
+      configFn = _: {
+        programs.niri = {
+          enable = true;
+          package = niriPackages.niri-unstable;
+        };
 
-      environment = {
-        systemPackages = with pkgs; [
-          wl-clipboard
-        ];
+        environment = {
+          systemPackages = with pkgs; [
+            wl-clipboard
+          ];
 
-        localBinInPath = true;
-        pathsToLink = [
-          "/share/applications"
-          "/share/xdg-desktop-portal"
-        ];
+          localBinInPath = true;
+          pathsToLink = [
+            "/share/applications"
+            "/share/xdg-desktop-portal"
+          ];
+        };
       };
-
-      nix.settings = {
-        substituters = ["https://niri.cachix.org"];
-        trusted-substituters = ["https://niri.cachix.org"];
-        trusted-public-keys = ["niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="];
-      };
-    };
-  }
+    }
