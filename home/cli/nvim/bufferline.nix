3cf0d16c19ad0037c73bea49b574ee191ca674d2
ErinaYip <erinayip@outlook.com>
2026-08-10 20:54:26 +0800

    feat(nvim): add bufferline binds

--- home/cli/nvim/bufferline.nix
+++ home/cli/nvim/bufferline.nix
@@ -1,24 +1,44 @@
-{mkKeymapd, ...}: {
+{
+  mkKeymaps,
+  mkKeymapd,
+  ...
+}: {
   tabline.nvimBufferline = {
     enable = true;
     setupOpts.options = {
-      numbers = "none";
+      numbers = "buffer_id";
       indicator = {
         style = "icon";
       };
+      separator_style = "slant";
+      hover.delay = 10;
     };
   };
 
-  keymaps = [
-    (mkKeymapd ["n"] "<leader>bp" "<cmd>BufferLineTogglePin<cr>" "[B]uffer [P]in")
-    (mkKeymapd ["n"] "<leader>bP" "<cmd>BufferLineGroupClose ungrouped<cr>" "[B]uffer delete non-pinned")
-    (mkKeymapd ["n"] "<leader>br" "<cmd>BufferLineCloseRight<cr>" "[B]uffer close right")
-    (mkKeymapd ["n"] "<leader>bl" "<cmd>BufferLineCloseLeft<cr>" "[B]uffer close left")
-    (mkKeymapd ["n"] "<S-h>" "<cmd>BufferLineCyclePrev<cr>" "Prev Buffer")
-    (mkKeymapd ["n"] "<S-l>" "<cmd>BufferLineCycleNext<cr>" "Next Buffer")
-    (mkKeymapd ["n"] "[b" "<cmd>BufferLineCyclePrev<cr>" "Prev Buffer")
-    (mkKeymapd ["n"] "]b" "<cmd>BufferLineCycleNext<cr>" "Next Buffer")
-    (mkKeymapd ["n"] "[B" "<cmd>BufferLineMovePrev<cr>" "Move buffer prev")
-    (mkKeymapd ["n"] "]B" "<cmd>BufferLineMoveNext<cr>" "Move buffer next")
-  ];
+  keymaps =
+    [
+      # (mkKeymapd ["n"] "<leader>bp" "<cmd>BufferLineTogglePin<cr>" "[B]uffer [P]in")
+      # (mkKeymapd ["n"] "<leader>bP" "<cmd>BufferLineGroupClose ungrouped<cr>" "[B]uffer delete non-pinned")
+      (mkKeymapd ["n"] "<leader>bd" "<cmd>bd<cr>" "[B]uffer [D]elete")
+      (mkKeymapd ["n"] "<leader>br" "<cmd>BufferLineCloseRight<cr>" "[B]uffer close [R]ight")
+      (mkKeymapd ["n"] "<leader>bl" "<cmd>BufferLineCloseLeft<cr>" "[B]uffer close [L]eft")
+      (mkKeymapd ["n"] "<leader>bo" "<cmd>BufferLineCloseOthers<cr>" "[B]uffer close [O]thers")
+      (mkKeymapd ["n"] "<S-h>" "<cmd>BufferLineCyclePrev<cr>" "Prev Buffer")
+      (mkKeymapd ["n"] "<S-l>" "<cmd>BufferLineCycleNext<cr>" "Next Buffer")
+      (mkKeymapd ["n"] "[b" "<cmd>BufferLineCyclePrev<cr>" "Prev Buffer")
+      (mkKeymapd ["n"] "]b" "<cmd>BufferLineCycleNext<cr>" "Next Buffer")
+      (mkKeymapd ["n"] "[B" "<cmd>BufferLineMovePrev<cr>" "Move buffer prev")
+      (mkKeymapd ["n"] "]B" "<cmd>BufferLineMoveNext<cr>" "Move buffer next")
+    ]
+    ++ (
+      builtins.concatLists (builtins.genList (
+          i: let
+            buffer = toString (i + 1);
+          in [
+            (mkKeymaps ["n"] "<leader>${buffer}" "<cmd>BufferLineGoToBuffer ${buffer}<cr>")
+            (mkKeymaps ["n"] "<leader>b${buffer}" "<cmd>bd ${buffer}<cr>")
+          ]
+        )
+        9)
+    );
 }
