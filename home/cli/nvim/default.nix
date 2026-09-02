282d677f48c6b10403f47988fcecb4bc64ede0b9
ErinaYip <erinayip@outlook.com>
2026-09-02 14:14:07 +0800

    feat(nvim): add MIME support fir neovim

--- home/cli/nvim/default.nix
+++ home/cli/nvim/default.nix
@@ -81,26 +81,51 @@ with eriniteLib;
         VISUAL = "nvim";
       };
 
-      xdg.desktopEntries = {
-        nvim = {
-          name = "Neovim";
-          genericName = "Text Editor";
-          exec = "kitty -e nvim %F";
-          icon = "nvim";
-          terminal = false;
-          type = "Application";
-          categories = ["Utility" "TextEditor" "Development"];
+      xdg = let
+        textMimeTypes = [
+          "application/x-code-workspace"
+          "application/x-shellscript"
+          "text/english"
+          "text/plain"
+          "text/x-c"
+          "text/x-c++"
+          "text/x-c++hdr"
+          "text/x-c++src"
+          "text/x-chdr"
+          "text/x-csrc"
+          "text/x-java"
+          "text/x-makefile"
+          "text/x-moc"
+          "text/x-pascal"
+          "text/x-tcl"
+          "text/x-tex"
+        ];
+      in {
+        desktopEntries = {
+          nvim = {
+            name = "Neovim";
+            genericName = "Text Editor";
+            exec = "kitty -e nvim %F";
+            icon = "nvim";
+            terminal = false;
+            type = "Application";
+            categories = ["Utility" "TextEditor" "Development"];
+            mimeType = textMimeTypes;
+          };
+
+          vim = {
+            name = "Vim";
+            genericName = "Text Editor";
+            exec = "kitty -e vim %F";
+            icon = "vim";
+            terminal = false;
+            type = "Application";
+            categories = ["Utility" "TextEditor" "Development"];
+            mimeType = textMimeTypes;
+          };
         };
 
-        vim = {
-          name = "Vim";
-          genericName = "Text Editor";
-          exec = "kitty -e vim %F";
-          icon = "vim";
-          terminal = false;
-          type = "Application";
-          categories = ["Utility" "TextEditor" "Development"];
-        };
+        mimeApps.defaultApplications = mkDefaultApplications "nvim.desktop" textMimeTypes;
       };
     };
   }
